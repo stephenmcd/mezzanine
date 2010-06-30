0363bb7854ffec61c8e001b6560bd99ff5aff383
stephenmcd <steve@jupo.org>
2010-07-01 06:36:39 +1000

    Add warning to mezzanine_project script to prevent the user from creating a project name that conflicts with an existing package or module.

--- mezzanine/bin/mezzanine_project.py
+++ mezzanine/bin/mezzanine_project.py
@@ -1,29 +1,49 @@
 #!/usr/bin/env python
 
 import os
-import sys
 import shutil
+import sys
+
 import mezzanine
 
-def create_project():
 
+class ProjectException(Exception):
+    pass
+    
+def create_project():
+    """
+    Copies the contents of the project_template directory to a new directory 
+    specified as an argument to the command line.
+    """
+
+    # Ensure a directory name is specified.
     script_name = os.path.basename(sys.argv[0])
     if len(sys.argv) == 1:
-        print
-        print "Usage: %s project_name" % script_name
-        print
-        sys.exit()
-        
+        raise ProjectException("Usage: %s project_name" % script_name)
+
+    # Ensure the given directory name doesn't clash with an existing Python 
+    # package/module.
+    try:
+        __import__(sys.argv[1])
+    except ImportError:
+        pass
+    else:
+        raise ProjectException("'%s' conflicts with the name of an existing "
+            "Python module and cannot be used as a project name. Please try "
+            "another name." % sys.argv[1])
+                    
     mezzanine_path = os.path.dirname(os.path.abspath(mezzanine.__file__))
     from_path = os.path.join(mezzanine_path, "project_template")
     to_path = os.path.join(os.getcwd(), sys.argv[1])
-
-    try:
-        shutil.copytree(from_path, to_path)
-    except Exception, e:
-        print
-        print "Error in %s: %s" % (script_name, e)
-        print
+    shutil.copytree(from_path, to_path)
         
 if __name__ == "__main__":
-    create_project()
+    try:
+        create_project()
+    except ProjectException, e:
+        print
+        print e
+        print
+        
+
+
