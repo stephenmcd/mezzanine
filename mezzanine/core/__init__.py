24ac7dce957c47abec48b2d65a2caeea5ab8e3c6
stephenmcd <steve@jupo.org>
2010-07-09 07:39:24 +1000

    Bugfix to tests loading and version bump.

--- mezzanine/core/__init__.py
+++ mezzanine/core/__init__.py
@@ -3,4 +3,3 @@ Provides abstract models and admin features used throughout the various
 Mezzanine apps.
 """
 
-from mezzanine import tests
---
+++ mezzanine/core/tests.py
@@ -0,0 +1,3 @@
+
+# Pick up the mezzanine tests since they're not in an installed app.
+from mezzanine.tests import *
