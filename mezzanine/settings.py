35ac50490a78338cc26456061daa42b1a2d771c9
Toby White <tow@eaddrinu.se>
2010-09-23 18:09:54 +0100

    Allow overriding CONTENT_MEDIA_URL

--- mezzanine/settings.py
+++ mezzanine/settings.py
@@ -55,8 +55,10 @@ setting("COMMENTS_NUM_LATEST", 5)
 setting("COMMENTS_UNAPPROVED_VISIBLE", True)
 
 # Media files for admin.
+# This should never be overridden
 CONTENT_MEDIA_PATH = os.path.join(os.path.dirname(__file__), "core", "media")
-CONTENT_MEDIA_URL = "/content_media/"
+# This might be overridden when mezzanine is used in a different project.
+settings(CONTENT_MEDIA_URL, "/content_media/")
 
 # Content status choices.
 CONTENT_STATUS_DRAFT = 1
