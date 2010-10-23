ad4cea9f35f6f9e1001a4e6d727e69768d39fe2d
stephenmcd <steve@jupo.org>
2010-10-23 11:02:11 +1100

    Make number of comments for a ``BlogPost`` available via ``BlogPostManager``.

--- mezzanine/blog/managers.py
+++ mezzanine/blog/managers.py
@@ -1,7 +1,18 @@
 
-from django.db.models import Manager
+from django.db.models import Count, Manager
 
 from mezzanine.settings import load_settings
+from mezzanine.core.managers import DisplayableManager
+
+
+class BlogPostManager(DisplayableManager):
+    """
+    Extends ``DisplayableManager.published`` with annotated comment counts.
+    """
+    
+    def published(self, *args, **kwargs):
+        return super(BlogPostManager, self).published(*args, **kwargs) \
+            .annotate(num_comments=Count("comments")).select_related(depth=1)
 
 
 class CommentManager(Manager):
