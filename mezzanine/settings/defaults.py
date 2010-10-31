cbe5b88c0259f3a4482362278a75aa9db8319873
stephenmcd <steve@jupo.org>
2010-10-31 12:11:21 +1100

    Make filebrowser menu item translatable.

--- mezzanine/settings/defaults.py
+++ mezzanine/settings/defaults.py
@@ -13,7 +13,7 @@ register_setting(
     editable=False,
     default=(
         (_("Content"), ("pages.Page", "blog.BlogPost", "blog.Comment",
-            ("Media Library", "fb_browse"),)),
+            (_("Media Library"), "fb_browse"),)),
         (_("Site"), ("sites.Site", "redirects.Redirect", "settings.Setting")),
         (_("Users"), ("auth.User", "auth.Group",)),
     ),
