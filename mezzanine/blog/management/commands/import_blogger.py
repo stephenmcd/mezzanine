c5acc0bad712228dba9fb6214729f2c1c2b6774f
ajfisher <ajfisher.td@gmail.com>
2010-11-21 17:41:08 +1100

    Further refactoring of the import process using a BaseImporterClass which is a Command and then setting up specific implementations for Wordpress and Blogger

---
+++ mezzanine/blog/management/commands/import_blogger.py
@@ -0,0 +1,136 @@
+from datetime import datetime, timedelta
+from optparse import make_option
+from time import mktime, strftime, strptime, timezone
+
+from django.core.management.base import CommandError
+
+from mezzanine.blog.management.base import BaseImporterCommand
+
+class Command(BaseImporterCommand):
+    """
+    Implements a Blogger importer. Takes a blogger ID in order to be able to
+    determine which blog it should point to and harvest the XML from
+    """   
+    gdata_url = "http://code.google.com/p/gdata-python-client/" 
+    
+    option_list = BaseImporterCommand.option_list + (
+        make_option("-b", "--blogger", dest="blog_id",
+            help="Blogger Blog ID from blogger dashboard"),
+    )
+    
+    def __init__(self):
+        """
+        Call the base command constructor so we have the right bindings
+        for errors
+        """
+        super(BaseImporterCommand, self).__init__()
+
+    def convert(self):
+        """
+        Gets posts from blogger and then formats them back into the standard
+        style ready for importation into Mezzanine.
+        """
+
+        blog_id = self.blog_id
+        server = "www.blogger.com"
+            
+        try:
+            from gdata import service
+            import gdata
+            import atom
+        except ImportError:
+            raise CommandError("You need to download the gdata python client module from %s to import blogger" % self.gdata_url)
+        
+        blogger = service.GDataService()
+        blogger.service = "blogger"
+        blogger.server = server
+        query = service.Query()
+        try:
+            query.feed = "/feeds/" + blog_id + "/posts/full"
+        except TypeError:
+            raise CommandError("You have not supplied a blog id for blogger")
+        query.max_results = 500
+	
+        try:
+            feed = blogger.Get(query.ToUri())
+        except gdata.service.RequestError, err:
+
+            message = "There was a service error. The response was: %(status)s %(reason)s - %(body)s" % err.message
+            raise FeedURLError(message, blogger.server + query.feed, err.message["status"])
+            return
+            
+        total_posts = len(feed.entry)
+	
+        print "Importing %s POSTS from blogger atom feed at %s" % (total_posts, query.feed)
+        
+        post_list = []
+        
+        for (i, entry) in enumerate(feed.entry):
+     
+            # this basically gets the unique post id from the URL to itself. Pulls
+            # the id off the end.
+            post_id = entry.GetSelfLink().href.split("/")[-1]
+
+            print "Processing post: %s/%s \n %s" % (i, total_posts, entry.title.text)
+            
+            title = entry.title.text
+            content = entry.content.text
+            #this strips off the time zone info off the end as we want UTC
+            published_date = datetime.strptime(entry.published.text[:-6], 
+                    "%Y-%m-%dT%H:%M:%S.%f") - timedelta(seconds = timezone)
+            
+            # get the tags
+            tags = [tag.term for tag in entry.category]
+            
+            #TODO - issues with content not generating correct <P> tags
+            
+
+            post = self.add_post(
+                title = title,
+                content = content,
+                pub_date = published_date,
+                tags = tags)
+            
+            # get the comments from the post feed and then add them to the post details
+            comment_url = "/feeds/" + blog_id + "/" + post_id + "/comments/full?max-results=1000"
+            comments = blogger.Get(comment_url)
+            
+            print "Comments %s" % len(comments.entry)
+            
+            for comment in comments.entry:
+                email = comment.author[0].email.text
+                author_name = comment.author[0].name.text
+                #this strips off the time zone info off the end as we want UTC
+                comment_date = datetime.strptime(comment.published.text[:-6], 
+                    "%Y-%m-%dT%H:%M:%S.%f") - timedelta(seconds = timezone)
+                website = ""
+                if comment.author[0].uri:
+                    website = comment.author[0].uri.text
+                body = comment.content.text
+                
+                # add the comment as a dict to the end of the comments list
+                self.add_comment(
+                    post = post,
+                    name = author_name,
+                    email = email,
+                    body = body,
+                    website = website,
+                    pub_date = comment_date)
+                
+    def handle(self, *args, **options):
+        """
+        Handle the import of a blogger blog into mezzanine
+        """
+        
+        self.blog_id = options["blog_id"]
+        self.mezzanine_user = options["mezzanine_user"]
+        
+        # handle a quick mezzanine user check here just to be nice
+        if self.mezzanine_user is None:
+            raise CommandError("Please specify a mezzanine user")
+        
+        self.convert()
+        self.process()
+        
+                    
+    
---
+++ mezzanine/blog/management/commands/import_wordpress.py
@@ -0,0 +1,153 @@
+from datetime import datetime, timedelta
+from optparse import make_option
+from time import mktime, strftime, strptime, timezone
+from xml.dom.minidom import parse, parseString
+
+from django.core.management.base import CommandError
+
+from mezzanine.blog.management.base import BaseImporterCommand
+
+class Command(BaseImporterCommand):
+    """
+    Implements a Wordpress importer.  Takes a file path or a URL in order
+    to point to the Wordpress Extended RSS file
+    """
+
+    option_list = BaseImporterCommand.option_list + (
+        make_option("-u", "--url", dest="url",
+            help="URL to import file"),
+    )
+
+    def __init__(self):
+        """
+        Call the base command constructor so we have the right bindings
+        for errors
+        """
+        super(BaseImporterCommand, self).__init__()
+
+    def get_text(self, xml, element, nodetype):
+        """
+        Gets the element's text value from the xml object provided, adapted from
+        minidom examples
+        """
+        rc = []
+        for node in xml.getElementsByTagName(element)[0].childNodes:
+            if node.nodeType == nodetype:
+                rc.append(node.data)
+        return "".join(rc)
+
+           
+    def convert(self):    
+        """
+        Gets the posts from either the provided URL or else
+        from the path if it is local and then formats them back into the standard
+        style ready for importation into Mezzanine. 
+        """
+    
+        url = self.url
+
+        try:
+            import feedparser
+        except ImportError:
+            raise CommndError("You need to downlod feedparser - try easy_install feedparser")
+
+        feed = feedparser.parse(url)
+
+        # we use the minidom parser as well because feedparser won't interpret
+        # WXR comments correctly and ends up munging them. Lightweight DOM parser is
+        # used simply to pull the comments when we get to them. If someone wants
+        # to rewrite this please do.
+        xml = parse(url)
+        xmlitems = xml.getElementsByTagName("item")
+
+        total_posts = len(feed["entries"])
+	
+        print "Importing %s POSTS from Wordpress RSS2.0 feed at %s" % (total_posts, url)
+
+
+        #i = 0 #counter for number of posts processed
+        
+        post_list = []
+        
+        for (i, entry) in enumerate(feed["entries"]):
+
+            print "Processing post: %s/%s \n%s" % (i+1, total_posts, entry.title)
+            
+            # get a pointer to the right position in the minidom as well.
+            xmlitem = xmlitems[i]
+            i = i + 1
+            
+            title = entry.title
+            content = entry.content[0]["value"]
+            content = "<p>".join([content.replace("\n\n", "</p><p>"), "</p>"])
+
+            # get the time struct of the published date if possible and the 
+            # updated date if we can"t.
+            try:
+                pd = entry.published_parsed
+            except AttributeError, err:
+                pd = entry.updated_parsed
+                
+            published_date = datetime.fromtimestamp(mktime(pd)) - timedelta(seconds = timezone)
+            
+            tags = [tag.term for tag in entry.tags if tag.scheme !="category"]
+            #tags have a tendency to not be unique in WP for some reason so
+            # set the list so we have unique
+            tags = list(set(tags))
+
+            comments_list = []
+
+            # create the temporary post object and append to the post_list
+            
+            post = self.add_post(
+                title = title,
+                content = content,
+                pub_date = published_date,
+                tags = tags)
+
+            # get the comments from the xml doc.
+            for comment in xmlitem.getElementsByTagName("wp:comment"):
+
+           
+                author_name = self.get_text(comment, "wp:comment_author", comment.CDATA_SECTION_NODE)
+                email = self.get_text(comment, "wp:comment_author_email", comment.TEXT_NODE)
+                website = ""
+                if self.get_text(comment, "wp:comment_author_url", comment.TEXT_NODE):
+                    website = self.get_text(comment, "wp:comment_author_url", comment.TEXT_NODE)
+                body = self.get_text(comment, "wp:comment_content", comment.CDATA_SECTION_NODE)
+
+
+                #use the GMT date (closest to UTC we'll end up with so this will
+                # make it relatively timezone fine format is YYYY-MM-DD HH:MM:SS
+                comment_date = datetime.strptime(
+                    self.get_text(comment, "wp:comment_date_gmt", comment.TEXT_NODE),
+                    "%Y-%m-%d %H:%M:%S" ) - timedelta(seconds = timezone)
+                
+                # add the comment as a dict to the end of the comments list
+                self.add_comment(
+                    post = post,
+                    name = author_name,
+                    email = email,
+                    body = body,
+                    website = website,
+                    pub_date = comment_date)
+                    
+                    
+    def handle(self, *args, **options):
+        """
+        Handle the import of a wordpress blog into mezzanine
+        """
+        
+        self.url = options["url"]
+        self.mezzanine_user = options["mezzanine_user"]
+        
+        if self.url is None:
+            raise CommandError("Please provide a URL or path to the import file")
+        # handle a quick mezzanine user check here just to be nice
+        if self.mezzanine_user is None:
+            raise CommandError("Please specify a mezzanine user")
+        
+        self.convert()
+        self.process()
+        
+        
