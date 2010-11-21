8346e01d24e1139f10973e8804ee2389220da430
stephenmcd <steve@jupo.org>
2010-11-22 07:17:49 +1100

    Initial version of ``install_theme`` command.

---
+++ mezzanine/core/management/commands/install_theme.py
@@ -0,0 +1,45 @@
+
+from distutils.dir_util import copy_tree
+import os
+
+from django.core.management.base import CommandError, LabelCommand
+
+from mezzanine.conf import settings
+from mezzanine.utils import path_for_import
+
+
+class Command(LabelCommand):
+    """
+    Copies the templates and media files from a theme package into 
+    the current project. 
+    """
+
+    option_list = AppCommand.option_list + (
+        make_option("--noinput", action="store_false", dest="interactive", 
+            help="Tells Django to NOT prompt the user for input of any kind.", 
+            default=True),
+    )
+    help = ("Copies the templates and media files from a theme package into "
+            "the current project.")
+    args = "[theme_name]"
+    label = "theme name"
+    can_import_settings = True
+
+    def handle_label(self, theme_name, **options):
+        """
+        Copy the templates and media files for the given theme package into 
+        the current project. 
+        """
+        try:
+            theme_path = path_for_import(theme_name)
+        except ImportError:
+            raise CommandError("Could not import the theme: %s" % theme_name)
+        copy_paths = (
+            (os.path.join(theme_path, "templates"), 
+                settings.TEMPLATE_DIRS[-1]),
+            (os.path.join(theme_path, "media"), 
+                settings.MEDIA_ROOT),
+        )
+        # TODO: Handle interactive option.
+        for (path_from, path_to) in copy_paths:
+            copy_tree(path_from, path_to)
