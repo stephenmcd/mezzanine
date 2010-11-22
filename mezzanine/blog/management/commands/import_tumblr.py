7e3e9851d8fbc1ea78a9f94340535caf03df77ac
stephenmcd <steve@jupo.org>
2010-11-22 14:48:59 +1100

    Add handling in the ``import_tumblr`` command for more posts that a single call to Tumblr's API allows.

--- mezzanine/blog/management/commands/import_tumblr.py
+++ mezzanine/blog/management/commands/import_tumblr.py
@@ -1,12 +1,18 @@
 
 from datetime import datetime
 from optparse import make_option
+from time import sleep
 from urllib import urlopen
 
 from django.core.management.base import CommandError
 from django.utils.simplejson import loads
 
 from mezzanine.blog.management.base import BaseImporterCommand
+
+
+MAX_POSTS_PER_CALL = 50 # Max number of posts Tumblr API will return per call.
+MAX_RETRIES_PER_CALL = 3 # Max times to retry API call after failing.
+SLEEP_PER_RETRY = 3 # Seconds to pause for between retries.
 
 
 class Command(BaseImporterCommand):
@@ -25,25 +31,46 @@ class Command(BaseImporterCommand):
         tumblr_user = options.get("tumblr_user")
         if tumblr_user is None:
             raise CommandError("Usage is import_tumblr %s" % self.args)
+        verbosity = int(options.get("verbosity", 1))
         json_url = "http://%s.tumblr.com/api/read/json" % tumblr_user
-        try:
-            response = urlopen(json_url)
-            if response.code == 404:
-                raise CommandError("Invalid Tumblr user.")
-            elif response.code == 503:
-                raise CommandError("Tumblr API currently unavailable, "
-                                   "try again shortly.")
-            elif response.code != 200:
-                raise IOError("HTTP status %s" % response.code)
-        except IOError, e:
-            raise CommandError("Error communicating with Tumblr API (%s)" % e)
-
-        start = "var tumblr_api_read ="
+        json_start = "var tumblr_api_read ="
         date_format = "%a, %d %b %Y %H:%M:%S"
-        json = loads(response.read().split(start, 1)[1].strip().rstrip(";"))
-        for entry in json["posts"]:
-            if entry["type"] == "regular":
-                published_date = datetime.strptime(entry["date"], date_format)
-                self.add_post(title=entry["regular-title"],
-                    content=entry["regular-body"], pub_date=published_date, 
-                    tags=entry.get("tags"), old_url=entry["url-with-slug"])
+        start_index = 0
+
+        while True:
+            retries = MAX_RETRIES_PER_CALL
+            try:
+                call_url = "%s?start=%s" % (json_url, start_index)
+                if verbosity >= 2:
+                    print "Calling %s" % call_url
+                response = urlopen(call_url)
+                if response.code == 404:
+                    raise CommandError("Invalid Tumblr user.")
+                elif response.code == 503:
+                    # The Tumblr API is frequently unavailable so make a 
+                    # few tries, pausing between each.
+                    retries -= 1
+                    if not retries:
+                        error = "Tumblr API unavailable, try again shortly."
+                        raise CommandError(error)
+                    sleep(3)
+                    continue
+                elif response.code != 200:
+                    raise IOError("HTTP status %s" % response.code)
+            except IOError, e:
+                error = "Error communicating with Tumblr API (%s)" % e
+                raise CommandError(error)
+
+            data = response.read()
+            json = loads(data.split(json_start, 1)[1].strip().rstrip(";"))
+            posts = json["posts"]
+            start_index += MAX_POSTS_PER_CALL
+
+            if not posts:
+                break
+            for post in posts:
+                if post["type"] == "regular":
+                    pub_date = datetime.strptime(post["date"], date_format)
+                    self.add_post(title=post["regular-title"],
+                        content=post["regular-body"], pub_date=pub_date, 
+                        tags=post.get("tags"), old_url=post["url-with-slug"])
