8fc01512df58423c41290529ee9415ce1e109898
stephenmcd <steve@jupo.org>
2010-12-01 21:14:11 +1100

    Renamed fixtures to not be installed with syncdb and added signal to install them when pages are first installed.
    --HG--
    rename : mezzanine/forms/fixtures/initial_data.json => mezzanine/forms/fixtures/mezzanine.json
    rename : mezzanine/pages/fixtures/initial_data.json => mezzanine/pages/fixtures/mezzanine.json

--- mezzanine/core/management/__init__.py
+++ mezzanine/core/management/__init__.py
@@ -2,7 +2,11 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import models as auth_app
+from django.core.management import call_command
 from django.db.models.signals import post_syncdb
+
+from mezzanine.pages.models import Page
+from mezzanine.pages import models as pages_app
 
 
 def create_demo_user(app, created_models, verbosity, **kwargs):
@@ -13,4 +17,23 @@ def create_demo_user(app, created_models, verbosity, **kwargs):
         User.objects.create_superuser("admin", "example@example.com", "default")
 
 
+def create_initial_pages(app, created_models, verbosity, **kwargs):
+    if settings.DEBUG and Page in created_models:
+        if kwargs.get("interactive"):
+            confirm = raw_input("\nWould you like to install some initial "
+                                "content?\nEg: About page, Blog, Contact " 
+                                "form. (yes/no): ")
+            while True:
+                if confirm == "yes":
+                    break
+                elif confirm == "no":
+                    return
+                confirm = raw_input("Please enter either 'yes' or 'no': ")
+        print 
+        print "Creating initial content (About page, Blog, Contact form)."
+        print 
+        call_command("loaddata", "mezzanine.json")
+
+
 post_syncdb.connect(create_demo_user, sender=auth_app)
+post_syncdb.connect(create_initial_pages, sender=pages_app)
