c6b44974501500987697d31b6253f0d682d0e22d
stephenmcd <steve@jupo.org>
2010-12-02 14:22:16 +1100

    Updated local_settings template.

--- mezzanine/project_template/settings.py
+++ mezzanine/project_template/settings.py
@@ -20,12 +20,18 @@ TEMPLATE_LOADERS = (
 # Databases.
 DATABASES = {
     "default": {
+        # "postgresql_psycopg2", "postgresql", "mysql", "sqlite3" or "oracle".
         "ENGINE": "",
-        "HOST": "",
+        # DB name or path to database file if using sqlite3.
         "NAME": "",
-        "PASSWORD": "",
-        "PORT": "",
+        # Not used with sqlite3.
         "USER": "",
+        # Not used with sqlite3.
+        "PASSWORD": "",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "",
+         # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
     }
 }
 
