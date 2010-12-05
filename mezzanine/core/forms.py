23f2ca4b37849959d64e3765deed3bfb7386ed35
stephenmcd <steve@jupo.org>
2010-12-05 12:13:48 +1100

    Added a split datetime widget with select fields for date parts in the inline editing form.

--- mezzanine/core/forms.py
+++ mezzanine/core/forms.py
@@ -2,6 +2,7 @@
 from uuid import uuid4
 
 from django import forms
+from django.forms.extras.widgets import SelectDateWidget
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
@@ -39,10 +40,26 @@ class DynamicInlineAdminForm(forms.ModelForm):
                 widget=OrderWidget, required=False)
 
 
+class SplitSelectDateTimeWidget(forms.SplitDateTimeWidget):
+    """
+    Combines Django's ``SelectDateTimeWidget`` and ``SelectDateWidget``.
+    """
+    def __init__(self, attrs=None, date_format=None, time_format=None):
+        date_widget = SelectDateWidget(attrs=attrs)
+        time_format = forms.SplitDateTimeWidget.time_format
+        time_widget = forms.TimeInput(attrs=attrs, format=time_format)
+        forms.MultiWidget.__init__(self, (date_widget, time_widget), attrs)
+
+
 def get_edit_form(obj, field_names, data=None, files=None):
     """
     Returns the in-line editing form for editing a single model field.
     """
+
+    formfield_overrides = (
+        (forms.DateField, SelectDateWidget),
+        (forms.DateTimeField, SplitSelectDateTimeWidget),
+    )
 
     class EditForm(forms.ModelForm):
         """
@@ -62,8 +79,12 @@ def get_edit_form(obj, field_names, data=None, files=None):
             super(EditForm, self).__init__(*args, **kwargs)
             self.uuid = str(uuid4())
             for f in self.fields.keys():
+                for (field_class, widget_class) in formfield_overrides:
+                    if isinstance(self.fields[f], field_class):
+                        attrs = {"class": field_class.__name__.lower()}
+                        self.fields[f].widget = widget_class(attrs=attrs)
                 self.fields[f].widget.attrs["id"] = "%s-%s" % (f, self.uuid)
 
-    initial = {"app": obj._meta.app_label, "id": obj.id, "fields": field_names, 
-        "model": obj._meta.object_name.lower()}
+    initial = {"app": obj._meta.app_label, "id": obj.id, 
+               "fields": field_names, "model": obj._meta.object_name.lower()}
     return EditForm(instance=obj, initial=initial, data=data, files=files)
