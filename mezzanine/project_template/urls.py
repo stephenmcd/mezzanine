dc5e0999c5cfbb97f919c648a19e7e849dbceb02
stephenmcd <steve@jupo.org>
2010-12-08 06:32:42 +1100

    Moved handling of serving assets during development from project's urlconf into ``mezzanine.urls``.

--- mezzanine/project_template/urls.py
+++ mezzanine/project_template/urls.py
@@ -1,11 +1,7 @@
 
-import os
 
 from django.conf.urls.defaults import *
 from django.contrib import admin
-
-from mezzanine.conf import settings
-from mezzanine.utils.path import path_for_import
 
 
 admin.autodiscover()
@@ -14,16 +10,3 @@ urlpatterns = patterns("",
     ("^admin/", include(admin.site.urls)),
     ("^", include("mezzanine.urls")),
 )
-
-if getattr(settings, "DEV_SERVER", False):
-    media_root = settings.MEDIA_ROOT
-    theme = getattr(settings, "THEME")
-    if theme:
-        media_root = os.path.join(path_for_import(theme), "media")
-    urlpatterns += patterns("",
-        ("^%s/(?P<path>.*)$" % settings.MEDIA_URL.strip("/"),
-            "django.views.static.serve", {"document_root": media_root}),
-        ("^favicon.ico$", 
-            "django.views.static.serve", {"document_root": media_root, 
-                                          "path": "img/favicon.ico"}),
-    )
--- mezzanine/urls.py
+++ mezzanine/urls.py
@@ -1,7 +1,10 @@
 
+import os
+
 from django.conf.urls.defaults import *
 
 from mezzanine.conf import settings
+from mezzanine.utils.path import path_for_import
 
 
 urlpatterns = patterns("",
@@ -15,4 +18,16 @@ if "mezzanine.blog" in settings.INSTALLED_APPS:
 if "mezzanine.pages" in settings.INSTALLED_APPS:
     urlpatterns += patterns("",
         ("^", include("mezzanine.pages.urls")),
+    )
+if getattr(settings, "DEV_SERVER", False):
+    media_root = settings.MEDIA_ROOT
+    theme = getattr(settings, "THEME")
+    if theme:
+        media_root = os.path.join(path_for_import(theme), "media")
+    urlpatterns += patterns("",
+        ("^%s/(?P<path>.*)$" % settings.MEDIA_URL.strip("/"),
+            "django.views.static.serve", {"document_root": media_root}),
+        ("^favicon.ico$", 
+            "django.views.static.serve", {"document_root": media_root, 
+                                          "path": "img/favicon.ico"}),
     )
