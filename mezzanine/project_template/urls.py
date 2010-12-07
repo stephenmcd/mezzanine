a9969d66525d95cd1cfdf1a641c8dcda55146e5a
stephenmcd <steve@jupo.org>
2010-12-08 07:26:41 +1100

    Refactored urls so that ``mezzanine.urls`` becomes the main point for combining urls for all the different apps. Also moved homepage url into the project's ``urlconf`` as it's expected to be modified.

--- mezzanine/project_template/urls.py
+++ mezzanine/project_template/urls.py
@@ -1,12 +1,17 @@
 
-
 from django.conf.urls.defaults import *
 from django.contrib import admin
+
+from mezzanine.core.views import direct_to_template
 
 
 admin.autodiscover()
 
+# Add the urlpatterns for any custom Django applications here. 
+# You can also change the ``home`` view to add your own functionality to 
+# the project's homepage.
 urlpatterns = patterns("",
     ("^admin/", include(admin.site.urls)),
+    url("^$", direct_to_template, {"template": "index.html"}, name="home"),
     ("^", include("mezzanine.urls")),
 )
