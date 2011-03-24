ddbb314d310f726e5027644630fdbb493daf9b41
legutierr <none@none>
2011-03-24 15:33:27 -0400

    added migrations for the new site field on Displayable.

---
+++ mezzanine/pages/migrations/0002_add_site.py
@@ -0,0 +1,70 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        def get_default_site():
+            return orm['sites.site']._default_manager.all().order_by("pk")[0]
+        
+        # Adding field 'Page.site'
+        db.add_column('pages_page', 'site', self.gf('django.db.models.fields.related.ForeignKey')(default=get_default_site(), to=orm['sites.Site']), keep_default=False)
+
+        # Changing field 'Page.description'
+        db.alter_column('pages_page', 'description', self.gf('django.db.models.fields.TextField')())
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Page.site'
+        db.delete_column('pages_page', 'site_id')
+
+        # Changing field 'Page.description'
+        db.alter_column('pages_page', 'description', self.gf('mezzanine.core.fields.HtmlField')())
+
+
+    models = {
+        'core.keyword': {
+            'Meta': {'object_name': 'Keyword'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'pages.contentpage': {
+            'Meta': {'ordering': "('_order',)", 'object_name': 'ContentPage', '_ormbases': ['pages.Page']},
+            'content': ('mezzanine.core.fields.HtmlField', [], {}),
+            'page_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['pages.Page']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'pages.page': {
+            'Meta': {'ordering': "('titles',)", 'object_name': 'Page'},
+            '_keywords': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            '_order': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
+            'content_model': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'expiry_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'in_footer': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'in_navigation': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'keywords': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['core.Keyword']", 'symmetrical': 'False', 'blank': 'True'}),
+            'login_required': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'children'", 'blank': 'True', 'null': 'True', 'to': "orm['pages.Page']"}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'short_url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'site': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['sites.Site']"}),
+            'slug': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'status': ('django.db.models.fields.IntegerField', [], {'default': '1'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'titles': ('django.db.models.fields.CharField', [], {'max_length': '1000', 'null': 'True'})
+        },
+        'sites.site': {
+            'Meta': {'ordering': "('domain',)", 'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['pages']
