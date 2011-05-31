80fed34d6906d92f98f3e1b0eeba09bbb59fa061
stephenmcd <steve@jupo.org>
2011-06-01 08:13:50 +1000

    Version bump.

--- mezzanine/__init__.py
+++ mezzanine/__init__.py
@@ -1,12 +1,2 @@
 
-from __future__ import with_statement
-import os
-
-
-requirements = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                            "project_template", "requirements", "project.txt")
-
-with open(requirements) as f:
-    for line in f:
-        if line.startswith("Mezzanine=="):
-            __version__ = line.split("==")[-1].strip()
+__version__ = "0.11.2"
