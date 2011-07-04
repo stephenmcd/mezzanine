33e01c9ce263fdd5d7f7d46f94482eda92f54f68
stephenmcd <steve@jupo.org>
2011-07-05 07:58:02 +1000

    Decorate blog posts in blog_post_list with lists of categories and keywords.

--- mezzanine/blog/views.py
+++ mezzanine/blog/views.py
@@ -1,14 +1,16 @@
 
 from calendar import month_name
+from collections import defaultdict
 
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 
 from mezzanine.blog.models import BlogPost, BlogCategory
 from mezzanine.conf import settings
-from mezzanine.generic.models import Keyword
+from mezzanine.generic.models import AssignedKeyword, Keyword
 from mezzanine.generic.utils import handle_comments
 from mezzanine.pages.models import RichTextPage
 from mezzanine.template.loader import select_template
@@ -53,7 +55,27 @@ def blog_post_list(request, tag=None, year=None, month=None, username=None,
         author = get_object_or_404(User, username=username)
         blog_posts = blog_posts.filter(user=author)
         templates.append("blog/blog_post_list_%s.html" % username)
-    blog_posts = paginate(blog_posts.select_related("user"),
+    # Create dicts mapping blog post IDs to lists of categories and
+    # keywords, and assign these to each blog post, to avoid querying
+    # the database inside the template loop for posts.
+    blog_posts = list(blog_posts.select_related("user"))
+    categories = defaultdict(list)
+    for cat in BlogCategory.objects.raw(
+        "SELECT * FROM blog_blogcategory "
+        "JOIN blog_blogpost_categories "
+        "ON blog_blogcategory.id = blog_blogpost_categories.blogcategory_id "
+        "WHERE blogpost_id IN (%s)" % ",".join([str(p.id) for p in blog_posts])):
+        categories[cat.blogpost_id].append(cat)
+    keywords = defaultdict(list)
+    blogpost_type = ContentType.objects.get(app_label="blog", model="blogpost")
+    assigned = AssignedKeyword.objects.filter(blogpost__in=blog_posts,
+                          content_type=blogpost_type).select_related("keyword")
+    for a in assigned:
+        keywords[a.object_pk].append(a.keyword)
+    for i, post in enumerate(blog_posts):
+        setattr(blog_posts[i], "category_list", categories[post.id])
+        setattr(blog_posts[i], "keyword_list", keywords[post.id])
+    blog_posts = paginate(blog_posts,
                           request.GET.get("page", 1),
                           settings.BLOG_POST_PER_PAGE,
                           settings.BLOG_POST_MAX_PAGING_LINKS)
