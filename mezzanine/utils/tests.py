7c84b83940b497133dda4616a158fa2e6199ecaa
stephenmcd <steve@jupo.org>
2011-09-23 12:07:53 +1000

    Upgraded pyflakes test to handle latest version of pyflakes.

--- mezzanine/utils/tests.py
+++ mezzanine/utils/tests.py
@@ -1,19 +1,17 @@
 
 from __future__ import with_statement
-from compiler import parse
+from _ast import PyCF_ONLY_AST
 import os
 
 from mezzanine.utils.importing import path_for_import
 
 
-# Ignore these warnings in pyflakes.
+# Ignore these warnings in pyflakes - if added to, please comment why.
 PYFLAKES_IGNORE = (
-    "import *' used",
-    "'memcache' imported but unused",
-    "'cmemcache' imported but unused",
-    "'__version__' imported but unused",
-    "redefinition of unused 'Feed'",
-    "redefinition of unused 'feed'",
+    "import *' used", # Required by Django's urlconf API.
+    "'__version__' imported but unused", # Used to version subpackages.
+    "redefinition of unused 'Feed'", # Compatibility for Django 1.3/1.4
+    "redefinition of unused 'feed'", # Compatibility for Django 1.3/1.4
 )
 
 
@@ -40,11 +38,12 @@ def run_pyflakes_for_package(package_name, extra_ignore=None):
             with open(path, "U") as source_file:
                 source = source_file.read()
             try:
-                compile(source, f, "exec")
+                tree = compile(source, f, "exec", PyCF_ONLY_AST)
             except (SyntaxError, IndentationError), value:
                 info = (path, value.lineno, value.args[0])
                 warnings.append("Invalid syntax in %s:%d: %s" % info)
-            result = Checker(parse(source), path)
+                continue
+            result = Checker(tree, path)
             for warning in result.messages:
                 message = unicode(warning)
                 for ignore in ignore_strings:
@@ -52,4 +51,5 @@ def run_pyflakes_for_package(package_name, extra_ignore=None):
                         break
                 else:
                     warnings.append(message)
+
     return warnings
