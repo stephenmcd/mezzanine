ece381a4e15ba9a7205f1e33f5b2796da6774fb4
stephenmcd <steve@jupo.org>
2011-09-24 21:49:57 +1000

    Renamed install command to createdb and added deprecation warning for install.

--- mezzanine/core/management/commands/install.py
+++ mezzanine/core/management/commands/install.py
@@ -1,36 +1,10 @@
 
-from django.core.management.base import NoArgsCommand
-from django.core.management.commands import syncdb
-
-from mezzanine.conf import settings
+from mezzanine.core.management.commands import createdb
 
 
-class Command(NoArgsCommand):
-
-    help = ("Performs initial Mezzanine installation by running Django's "
-            "syncdb and South's migrations faked.")
-    can_import_settings = True
-    option_list = syncdb.Command.option_list
+class Command(createdb.Command):
 
     def handle_noargs(self, **options):
-        verbosity = int(options.get('verbosity', 0))
-        syncdb.Command().execute(**options)
-        if settings.USE_SOUTH:
-            try:
-                from south.management.commands import migrate
-            except ImportError:
-                return
-            if options.get("interactive"):
-                confirm = raw_input("\nWould you like to fake initial "
-                                    "migrations? (yes/no): ")
-                while True:
-                    if confirm == "yes":
-                        break
-                    elif confirm == "no":
-                        return
-                    confirm = raw_input("Please enter either 'yes' or 'no': ")
-            if verbosity >= 1:
-                print
-                print "Faking initial migrations ..."
-                print
-            migrate.Command().execute(fake=True)
+        from warnings import warn
+        warn("install is deprecated; use: ./manage.py createdb")
+        super(Command, self).handle_noargs(**options)
