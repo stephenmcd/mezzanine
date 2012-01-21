23b3a48489fe8cd81aeedd84e5c56343d324963c
Stephen McDonald <steve@jupo.org>
2012-01-22 01:13:47 +1100

    Added a replacement post_syncdb handler for the sites app that points to runserver's default host:port.

--- mezzanine/core/management/__init__.py
+++ mezzanine/core/management/__init__.py
@@ -2,6 +2,9 @@
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import models as auth_app
+from django.contrib.sites.models import Site
+from django.contrib.sites.management import create_default_site
+from django.contrib.sites import models as sites_app
 from django.core.management import call_command
 from django.db.models.signals import post_syncdb
 
@@ -24,7 +27,8 @@ def create_user(app, created_models, verbosity, interactive, **kwargs):
 
 
 def create_pages(app, created_models, verbosity, interactive, **kwargs):
-    if settings.DEBUG and Page in created_models and Form in created_models:
+    required = set([Page, Form])
+    if settings.DEBUG and required.issubset(set(created_models)):
         if interactive:
             confirm = raw_input("\nWould you like to install some initial "
                                 "content?\nEg: About page, Blog, Contact "
@@ -43,5 +47,17 @@ def create_pages(app, created_models, verbosity, interactive, **kwargs):
         call_command("loaddata", "mezzanine.json")
 
 
+def create_site(app, created_models, verbosity, interactive, **kwargs):
+    if settings.DEBUG and Site in created_models:
+        domain = "127.0.0.1:8000"
+        if verbosity >= 1:
+            print
+            print "Creating default Site %s ... " % domain
+            print
+        Site.objects.create(name="Local development", domain=domain)
+
+
 post_syncdb.connect(create_user, sender=auth_app)
 post_syncdb.connect(create_pages, sender=pages_app)
+post_syncdb.connect(create_site, sender=sites_app)
+post_syncdb.disconnect(create_default_site, sender=sites_app)
