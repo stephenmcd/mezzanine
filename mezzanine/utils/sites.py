d29ab4f7e90a561adf478e1dee326d21a63842bb
Stephen McDonald <steve@jupo.org>
2012-03-06 06:47:43 +1100

    More pep8 fixes - unused imports.

--- mezzanine/utils/sites.py
+++ mezzanine/utils/sites.py
@@ -1,11 +1,13 @@
 
 import os
 
+from mezzanine.conf import settings
+
+
 def host_theme_path(request):
     """
     Returns the directory of the theme associated with the given host.
     """
-    from mezzanine.conf import settings
     for (host, theme) in settings.HOST_THEMES:
         if host.lower() == request.get_host().split(":")[0].lower():
             try:
@@ -16,13 +18,13 @@ def host_theme_path(request):
                 return os.path.dirname(os.path.abspath(module.__file__))
     return ""
 
+
 def templates_for_host(request, templates):
     """
     Given a template name (or list of them), returns the template names
     as a list, with each name prefixed with the device directory
     inserted into the front of the list.
     """
-    from mezzanine.conf import settings
     if not isinstance(templates, (list, tuple)):
         templates = [templates]
     theme_dir = host_theme_path(request)
