c737bd41d049129bdaa1af0fa063f64c1b4f3061
Stephen McDonald <steve@jupo.org>
2012-03-17 15:56:07 +1100

    Added more info and examples of different homepage patterns in project_template/urls.py.

--- mezzanine/project_template/urls.py
+++ mezzanine/project_template/urls.py
@@ -10,25 +10,59 @@ admin.autodiscover()
 # Add the urlpatterns for any custom Django applications here.
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
+
 urlpatterns = patterns("",
     ("^admin/", include(admin.site.urls)),
 
-    # Next pattern is the home view - defaults to the template
-    # index.html, but if you'd like the homepage to be your blog,
-    # remove the pattern below, and use the one commented out underneath
-    # it. You'll also need to set BLOG_SLUG = "" in your settings.py
-    # module, and delete the blog page object if it was installed,
-    # to get this working
+    # We don't want to presume how your homepage works, so here are a
+    # few patterns you can use to set it up.
+
+    # HOMEPAGE AS STATIC TEMPLATE
+    # ---------------------------
+    # This pattern simply loads the index.html template. It isn't
+    # commented out like the others, so it's the default. You only need
+    # one homepage pattern, so if you use a different one, comment this
+    # one out.
+
     url("^$", direct_to_template, {"template": "index.html"}, name="home"),
+
+    # HOMEPAGE AS AN EDITABLE PAGE IN THE PAGE TREE
+    # ---------------------------------------------
+    # This pattern gives us a normal ``Page`` object, so that your
+    # homepage can be managed via the page tree in the admin. If you
+    # use this pattern, you'll need to create a page in the page tree,
+    # and specify its URL (in the Meta Data section) as "home", which
+    # is the name used below in the ``{"slug": "home"}`` part. Make
+    # sure to uncheck "show in navigation" when you create the page,
+    # since the link to the homepage is always hard-coded into all the
+    # page menus that display navigation on the site.
+
+    # url("^$", "mezzanine.pages.views.page", {"slug": "home"}, name="home"),
+
+    # HOMEPAGE FOR A BLOG-ONLY SITE
+    # -----------------------------
+    # This pattern points the homepage to the blog post listing page,
+    # and is useful for sites that are primarily blogs. If you use this
+    # pattern, you'll also need to set BLOG_SLUG = "" in your
+    # ``settings.py`` module, and delete the blog page object from the
+    # page tree in the admin if it was installed
+
     # url("^$", "mezzanine.blog.views.blog_post_list", name="home"),
 
-    # ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
+    # MEZZANINE'S URLS
+    # ----------------
+    # Note: ADD YOUR OWN URLPATTERNS *ABOVE* THE LINE BELOW.
     # ``mezzanine.urls`` INCLUDES A *CATCH ALL* PATTERN
     # FOR PAGES, SO URLPATTERNS ADDED BELOW ``mezzanine.urls``
     # WILL NEVER BE MATCHED!
+    # If you'd like more granular control over the patterns in
+    # ``mezzanine.urls``, go right ahead and take the parts you want
+    # from it, and use them directly below instead of using
+    # ``mezzanine.urls``.
     ("^", include("mezzanine.urls")),
 
 )
 
-# Adds ``STATIC_URL`` to the context.
+# Adds ``STATIC_URL`` to the context of error pages, so that error
+# pages can use JS, CSS and images.
 handler500 = "mezzanine.core.views.server_error"
