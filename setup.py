7a72ec04451524e84e9b96524cbfadb1395487a9
Stephen McDonald <steve@jupo.org>
2012-03-19 21:35:41 +1100

    Bump dependencies.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from mezzanine import __version__ as version
 
 install_requires = [
     "django >= 1.3.1",
-    "filebrowser_safe == 0.2.1",
+    "filebrowser_safe == 0.2.2",
     "grappelli_safe == 0.2.2",
 ]
 
