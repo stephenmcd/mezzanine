967cd3cea8295fd30e4b4693abd33f03aa91e591
Stephen McDonald <steve@jupo.org>
2012-03-26 08:19:01 +1100

    Correctly handle model field defaults in the quick blog post form.

--- mezzanine/blog/forms.py
+++ mezzanine/blog/forms.py
@@ -2,7 +2,13 @@
 from django import forms
 
 from mezzanine.blog.models import BlogPost
-from mezzanine.core.models import CONTENT_STATUS_DRAFT
+
+
+# These fields need to be in the form, hidden, with default values,
+# since it posts to the blog post admin, which includes these fields
+# and will use empty values instead of the model defaults, without
+# these specified.
+hidden_field_defaults = ("status", "gen_description", "allow_comments")
 
 
 class BlogPostForm(forms.ModelForm):
@@ -13,10 +19,12 @@ class BlogPostForm(forms.ModelForm):
 
     class Meta:
         model = BlogPost
-        fields = ("title", "content", "status", "gen_description")
+        fields = ("title", "content") + hidden_field_defaults
 
     def __init__(self):
-        initial = {"status": CONTENT_STATUS_DRAFT, "gen_description": True}
+        initial = {}
+        for field in hidden_field_defaults:
+            initial[field] = BlogPost._meta.get_field(field).default
         super(BlogPostForm, self).__init__(initial=initial)
-        self.fields["status"].widget = forms.HiddenInput()
-        self.fields["gen_description"].widget = forms.HiddenInput()
+        for field in hidden_field_defaults:
+            self.fields[field].widget = forms.HiddenInput()
