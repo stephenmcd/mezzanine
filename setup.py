4db8567c83a071e50b9f9b325086a85ab3d6d537
Stephen McDonald <steve@jupo.org>
2012-04-02 21:26:13 +1000

    Fix grappelli version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from mezzanine import __version__ as version
 install_requires = [
     "django >= 1.3.1",
     "filebrowser_safe == 0.2.4",
-    "grappelli_safe == 0.2.4",
+    "grappelli_safe == 0.2.3",
 ]
 
 try:
