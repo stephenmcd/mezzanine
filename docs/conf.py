96915c33325fd74277a630c27069e4c92482e951
Stephen McDonald <steve@jupo.org>
2012-04-16 21:59:21 +1000

    Dev started in 2009.

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Mezzanine'
-copyright = u'2010 - 2012, Stephen McDonald'
+from datetime import datetime
+copyright = u'2009 - %s, Stephen McDonald' % datetime.now().year
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
