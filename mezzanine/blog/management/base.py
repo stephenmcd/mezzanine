8641bfd49efa251998735d9257007ff3be7e64ad
Alvin Mites <alvin@mitesdesign.com>
2012-04-19 16:14:54 -0600

    blog import - ignore 0 length category names

--- mezzanine/blog/management/base.py
+++ mezzanine/blog/management/base.py
@@ -189,9 +189,10 @@ class BaseImporterCommand(BaseCommand):
             for name in categories:
                 name = self.trunc_for_model(name,
                                 BlogCategory, field='title', user_input=name)
-                cat, created = BlogCategory.objects.get_or_create(title=name)
-                print "Importing Category by: %s" % cat
-                post.categories.add(cat)
+                if len(name) > 0:
+                    cat, created = BlogCategory.objects.get_or_create(title=name)
+                    print "Importing Category by: %s" % cat
+                    post.categories.add(cat)
 
             for comment in comments:
                 if verbosity >= 1:
