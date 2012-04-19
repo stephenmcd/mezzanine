f9ae1798bb9a214edbc7af2f7e80a4e42c954d55
Alvin Mites <alvin@mitesdesign.com>
2012-04-19 12:11:49 -0600

    Added page import to wordpress

--- mezzanine/blog/management/commands/import_wordpress.py
+++ mezzanine/blog/management/commands/import_wordpress.py
@@ -1,10 +1,12 @@
 
 from datetime import datetime, timedelta
 from optparse import make_option
+import re
 from time import mktime, timezone
 from xml.dom.minidom import parse
 
 from django.core.management.base import CommandError
+from django.utils.html import linebreaks
 
 from mezzanine.blog.management.base import BaseImporterCommand
 
@@ -59,7 +61,8 @@ class Command(BaseImporterCommand):
             xmlitem = xmlitems[i]
             title = entry.title
             content = entry.content[0]["value"]
-            content = "<p>".join([content.replace("\n\n", "</p><p>"), "</p>"])
+#            content = "<p>".join([content.replace("\n\n", "</p><p>"), "</p>"])
+            content = linebreaks(self.wp_caption(content))
 
             # get the time struct of the published date if possible and the
             # updated date if we can"t.
@@ -92,31 +95,62 @@ class Command(BaseImporterCommand):
             else:
                 cats = []
 
-            # create the post
-            post = self.add_post(title=title, content=content,
+            if entry.wp_post_type == 'post':
+                # create the post
+                post = self.add_post(title=title, content=content,
+                                     pub_date=published_date, tags=tags,
+                                     categories=cats)
+
+                # get the comments from the xml doc.
+                for comment in xmlitem.getElementsByTagName("wp:comment"):
+                    author_name = self.get_text(comment, "wp:comment_author",
+                                                comment.CDATA_SECTION_NODE)
+                    email = self.get_text(comment, "wp:comment_author_email",
+                                          comment.TEXT_NODE)
+                    website = ""
+                    if self.get_text(comment, "wp:comment_author_url",
+                                     comment.TEXT_NODE):
+                        website = self.get_text(comment, "wp:comment_author_url",
+                                                comment.TEXT_NODE)
+                    body = self.get_text(comment, "wp:comment_content",
+                                         comment.CDATA_SECTION_NODE)
+
+                    comment_date = self.get_text(comment, "wp:comment_date_gmt",
+                                                 comment.TEXT_NODE)
+                    comment_date = datetime.strptime(comment_date,
+                                                     "%Y-%m-%d %H:%M:%S")
+                    comment_date -= timedelta(seconds=timezone)
+
+                    # add the comment as a dict to the end of the comments list
+                    self.add_comment(post=post, name=author_name, email=email,
+                        body=body, website=website, pub_date=comment_date)
+
+            elif entry.wp_post_type == 'page':
+                old_id = getattr(entry, 'wp_post_id', False)
+                parent_id = getattr(entry, 'wp_post_parent', False)
+
+                page = self.add_page(title=title, content=content,
                                  pub_date=published_date, tags=tags,
-                                 categories=cats)
+                                 old_id=old_id, old_parent_id=parent_id)
 
-            # get the comments from the xml doc.
-            for comment in xmlitem.getElementsByTagName("wp:comment"):
-                author_name = self.get_text(comment, "wp:comment_author",
-                                            comment.CDATA_SECTION_NODE)
-                email = self.get_text(comment, "wp:comment_author_email",
-                                      comment.TEXT_NODE)
-                website = ""
-                if self.get_text(comment, "wp:comment_author_url",
-                                 comment.TEXT_NODE):
-                    website = self.get_text(comment, "wp:comment_author_url",
-                                            comment.TEXT_NODE)
-                body = self.get_text(comment, "wp:comment_content",
-                                     comment.CDATA_SECTION_NODE)
-
-                comment_date = self.get_text(comment, "wp:comment_date_gmt",
-                                             comment.TEXT_NODE)
-                comment_date = datetime.strptime(comment_date,
-                                                 "%Y-%m-%d %H:%M:%S")
-                comment_date -= timedelta(seconds=timezone)
-
-                # add the comment as a dict to the end of the comments list
-                self.add_comment(post=post, name=author_name, email=email,
-                    body=body, website=website, pub_date=comment_date)
+    def wp_caption(self, post):
+        '''
+        Filters a Wordpress Post for Image Captions
+        and renders to match HTML.
+        '''
+        for match in re.finditer(r"\[caption (.*?)\](.*?)\[/caption\]", post):
+            meta = '<div '
+            caption = ''
+            for imatch in re.finditer(r'(\w+)="(.*?)"', match.group(1)):
+                if imatch.group(1) == 'id':
+                    meta += 'id="%s" ' % imatch.group(2)
+                if imatch.group(1) == 'align':
+                    meta += 'class="wp-caption %s" ' % imatch.group(2)
+                if imatch.group(1) == 'width':
+                    meta += 'style="width: %spx;" ' % str(int(imatch.group(2)) + 10)
+                if imatch.group(1) == 'caption':
+                    caption = imatch.group(2)
+            meta += '>%s<p class="wp-caption-text">%s</p></div>' % \
+                (match.group(2), caption)
+            post = post.replace(match.group(0), meta)
+        return post
