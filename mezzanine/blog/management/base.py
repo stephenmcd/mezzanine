e794f0667514b1438b79e719b3d009cbf8d6a281
Stephen McDonald <steve@jupo.org>
2012-04-22 14:37:45 +1000

    Cleaned up the blog importers: - Gave the new nav options proper shortcuts - Added missing docstrings/comments - Refactor the trunc methods to remove large exception block which was trapping unknown errors - Only prompt for trunc when in interactive mode - Lots of pep8 fixes

--- mezzanine/blog/management/base.py
+++ mezzanine/blog/management/base.py
@@ -8,6 +8,9 @@ from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand, CommandError
 from django.utils.html import strip_tags
 
+from mezzanine.blog.models import BlogPost, BlogCategory
+from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
+from mezzanine.generic.models import AssignedKeyword, Keyword, ThreadedComment
 from mezzanine.pages.models import RichTextPage
 from mezzanine.utils.html import decode_entities
 
@@ -23,10 +26,13 @@ class BaseImporterCommand(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option("-m", "--mezzanine-user", dest="mezzanine_user",
             help="Mezzanine username to assign the imported blog posts to."),
-        make_option('--nav', action='store_true', dest='in_navigation',
-            help='Add pages to navigation'),
-        make_option('--foot', action='store_true', dest='in_footer',
-            help='Add pages to footer navigation'),
+        make_option("--noinput", action="store_false", dest="interactive",
+            default=True, help="Do NOT prompt for input of any kind. "
+                               "Fields will be truncated if too long."),
+        make_option("-n", "--navigation", action="store_true",
+            dest="in_navigation", help="Add any imported pages to navigation"),
+        make_option("-f", "--footer", action="store_true", dest="in_footer",
+            help="Add any imported pages to footer navigation"),
     )
 
     def __init__(self, **kwargs):
@@ -34,13 +40,17 @@ class BaseImporterCommand(BaseCommand):
         self.pages = []
         super(BaseImporterCommand, self).__init__(**kwargs)
 
-    def add_post(self, title=None, pub_date=None, tags=None,
-        content=None, comments=None, old_url=None, categories=None):
+    def add_post(self, title=None, content=None, old_url=None, pub_date=None,
+                 tags=None, categories=None, comments=None):
         """
         Adds a post to the post list for processing.
 
-        Attributes:
-            pub_date is assumed to be a datetime object.
+        - ``title`` and ``content`` are strings for the post.
+        - ``old_url`` is a string that a redirect will be created for.
+        - ``pub_date`` is assumed to be a ``datetime`` object.
+        - ``tags`` and ``categories`` are sequences of strings.
+        - ``comments`` is a sequence of dicts - each dict should be the
+          return value of ``add_comment``.
         """
         if not title:
             title = decode_entities(strip_tags(content).split(". ")[0])
@@ -61,30 +71,30 @@ class BaseImporterCommand(BaseCommand):
         })
         return self.posts[-1]
 
-    def add_page(self, title=None, pub_date=None, tags=None,
-         content=None, old_url=None, old_id=None, old_parent_id=None):
+    def add_page(self, title=None, content=None, old_url=None,
+                 tags=None, old_id=None, old_parent_id=None):
+        """
+        Adds a page to the list of pages to be imported - used by the
+        Wordpress importer.
+        """
         if not title:
-            title = decode_entities(strip_tags(content).split(". ")[0])
+            text = decode_entities(strip_tags(content)).replace("\n", " ")
+            title = text.split(". ")[0]
         if tags is None:
             tags = []
         self.pages.append({
-            "title": title.replace('\n', ' ')[:RichTextPage._meta.get_field('title').max_length - 1],
-            "publish_date": pub_date,
+            "title": title,
             "content": content,
             "tags": tags,
             "old_url": old_url,
-            "old_id" : old_id,
-            "old_parent_id" : old_parent_id,
+            "old_id": old_id,
+            "old_parent_id": old_parent_id,
         })
-        return self.pages[-1]
 
     def add_comment(self, post=None, name=None, email=None, pub_date=None,
-        website=None, body=None):
+                    website=None, body=None):
         """
         Adds a comment to the post provided.
-
-        Attributes:
-            pub_date is assumed to be a date time object.
         """
         if post is None:
             if not self.posts:
@@ -98,52 +108,27 @@ class BaseImporterCommand(BaseCommand):
             "comment": body,
         })
 
-    def trunc_for_model_field(self, string, model, field, user_input=False):
-        '''
-        Truncates a string for a model, in the format.
-        optional user_input = string to display to user to decide to truncate
-        '''
-        try:
-            if model._meta.get_field(field).max_length and len(string) > model._meta.get_field(field).max_length:
-#                print len(string)
-#                print model._meta.get_field(field).max_length
-#                print field
-                if user_input:
-                    continue_words = set(['y', 'yes'])
-                    change_words = set(['c', 'change'])
-                    new_str = string[:model._meta.get_field(field).max_length-1]
-                    do_trunc = raw_input('Would you like to truncate the : %s for model %s:\n to: %s (y[es]/n[o]/c[hange]):  ' % (user_input, str(model), new_str))
-                    if not continue_words.intersection(do_trunc.lower()):
-                        if change_words.intersection(do_trunc.lower()):
-                            return raw_input('Enter new title - max length = %s:  ' % model._meta.get_field(field).max_length)
-                        else:
-                            return False
-                string = new_str
-        except TypeError:
-            pass
-        return string
-
-    def trunc_for_model(self, obj, model, **kwargs):
-        '''
-        Truncates a string or dict values for a model, in the format:
-          'var' : 'value'
-        Where var is both a dict value, and model field.
-        optional kwargs:
-          user_input = 'title to display for user option'
-          field = single dict field to truncate
-        '''
-        field = kwargs.get('field', False)
-        if type(obj) == str:
-            if not field:
-                raise Exception('truncating string for model requires kwarg field to be set')
-            return self.trunc_for_model_field(obj, model, **kwargs)
-        elif type(obj) == dict:
-            if kwargs.get('field', False):
-                obj[field] = self.trunc_for_model_field(obj[field], model, field, **kwargs)
-            else:
-                for var, val in obj.iteritems():
-                    obj[var] = self.trunc_for_model_field(val, model, var, **kwargs)
-            return obj
+    def trunc(self, model, prompt, **fields):
+        """
+        Truncates fields values for the given model. Prompts for a new
+        value if truncation occurs.
+        """
+        for field_name, value in fields.items():
+            field = model._meta.get_field(field_name)
+            max_length = getattr(field, "max_length", None)
+            if not max_length:
+                continue
+            elif not prompt:
+                fields[field_name] = value[:max_length]
+            while len(value) > max_length:
+                encoded_value = value.encode("utf-8")
+                new_value = raw_input("The value for the field %s.%s exceeds "
+                    "its maximum length of %s chars: %s\n\nEnter a new value "
+                    "for it, or press return to have it truncated: " %
+                    (model.__name__, field_name, max_length, encoded_value))
+                value = new_value if new_value else value[:max_length]
+            fields[field_name] = value
+        return fields
 
     def handle(self, *args, **options):
         """
@@ -154,13 +139,10 @@ class BaseImporterCommand(BaseCommand):
             date_format: the format the dates are in for posts and comments
         """
 
-        from mezzanine.blog.models import BlogPost, BlogCategory
-        from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
-        from mezzanine.generic.models import ThreadedComment
-
         mezzanine_user = options.get("mezzanine_user")
         site = Site.objects.get_current()
         verbosity = int(options.get("verbosity", 1))
+        prompt = options.get("interactive")
 
         # Validate the Mezzanine user.
         if mezzanine_user is None:
@@ -170,90 +152,85 @@ class BaseImporterCommand(BaseCommand):
         except User.DoesNotExist:
             raise CommandError("Invalid Mezzanine user: %s" % mezzanine_user)
 
-        # Run the subclassed ``handle_import`` and save posts, tags and
-        # comments to the DB.
+        # Run the subclassed ``handle_import`` and save posts, tags,
+        # categories, and comments to the DB.
         self.handle_import(options)
         for post in self.posts:
-
-            if verbosity >= 1:
-                print "Importing post titled: %s" % post["title"]
             categories = post.pop("categories")
             tags = post.pop("tags")
             comments = post.pop("comments")
             old_url = post.pop("old_url")
-            post_args = self.trunc_for_model(post, BlogPost, user_input=post['title'])
-            post_args["user"] = mezzanine_user
-            post_args["status"] = CONTENT_STATUS_PUBLISHED
-            post, created = BlogPost.objects.get_or_create(**post_args)
-
+            post = self.trunc(BlogPost, prompt, **post)
+            post["user"] = mezzanine_user
+            post["status"] = CONTENT_STATUS_PUBLISHED
+            post, created = BlogPost.objects.get_or_create(**post)
+            if created and verbosity >= 1:
+                print "Imported post: %s" % post
             for name in categories:
-                name = self.trunc_for_model(name,
-                                BlogCategory, field='title', user_input=name)
-                if len(name) > 0:
-                    cat, created = BlogCategory.objects.get_or_create(title=name)
-                    print "Importing Category by: %s" % cat
-                    post.categories.add(cat)
-
+                cat = self.trunc(BlogCategory, prompt, title=name)
+                if not cat["title"]:
+                    continue
+                cat, created = BlogCategory.objects.get_or_create(**cat)
+                if created and verbosity >= 1:
+                    print "Imported category: %s" % cat
+                post.categories.add(cat)
             for comment in comments:
-                if verbosity >= 1:
-                    print "Importing comment by: %s" % comment["user_name"]
-
-                comment = self.trunc_for_model(comment,
-                    ThreadedComment, user_input='Comment from %s' % comment["user_name"])
+                comment = self.trunc(ThreadedComment, prompt, **comment)
                 comment["site"] = site
                 post.comments.add(ThreadedComment(**comment))
-            self.add_meta(post, tags, old_url)
+                if verbosity >= 1:
+                    print "Imported comment by: %s" % comment["user_name"]
+            self.add_meta(post, tags, prompt, verbosity, old_url)
 
-        in_footer = options.get('in_footer', False)
-        if not in_footer:
-            in_footer = False
-        in_navigation = options.get('in_navigation', False)
-        if not in_navigation:
-            in_navigation = False
+        # Create any pages imported (Wordpress can include pages)
         parents = []
-#        print 'in_footer = %s' % in_footer
-#        print 'in_navigation = %s' % in_navigation
         for page in self.pages:
-            if verbosity >= 1:
-                print "Importing page titled: %s" % page["title"]
             tags = page.pop("tags")
             old_url = page.pop("old_url")
             old_id = page.pop("old_id")
             old_parent_id = page.pop("old_parent_id")
-            page_args = page
-            page_args["status"] = CONTENT_STATUS_PUBLISHED
-            page_args['in_navigation'] = in_navigation
-            page_args['in_footer'] = in_footer
-            page, created = RichTextPage.objects.get_or_create(**page_args)
-            self.add_meta(page, tags, old_url)
-
+            page = self.trunc(RichTextPage, prompt, **page)
+            page["status"] = CONTENT_STATUS_PUBLISHED
+            page["in_navigation"] = options["in_navigation"] or False
+            page["in_footer"] = options["in_footer"] or False
+            page, created = RichTextPage.objects.get_or_create(**page)
+            if created and verbosity >= 1:
+                print "Imported page: %s" % page
+            self.add_meta(page, tags, prompt, verbosity, old_url)
             parents.append({
-                'old_id' : old_id,
-                'old_parent_id' : old_parent_id,
-                'page' : page,
+                'old_id': old_id,
+                'old_parent_id': old_parent_id,
+                'page': page,
             })
 
         for obj in parents:
-#            print '%s : %s : %s : %s' % (obj['old_id'], obj['old_parent_id'], obj['page'].id, obj['page'].title)
             if obj['old_parent_id']:
                 for parent in parents:
                     if parent['old_id'] == obj['old_parent_id']:
                         obj['page'].parent = parent['page']
                         obj['page'].save()
-                        continue
+                        break
 
-    def add_meta(self, post, tags, old_url=None):
-        from mezzanine.generic.models import AssignedKeyword, Keyword
-
+    def add_meta(self, obj, tags, prompt, verbosity, old_url=None):
+        """
+        Adds tags and a redirect for the given obj, which is a blog
+        post or a page.
+        """
         for tag in tags:
-            keyword, created = Keyword.objects.get_or_create(title=tag)
-            post.keywords.add(AssignedKeyword(keyword=keyword))
-
+            keyword = self.trunc(Keyword, prompt, title=tag)
+            keyword, created = Keyword.objects.get_or_create(**keyword)
+            obj.keywords.add(AssignedKeyword(keyword=keyword))
+            if created and verbosity >= 1:
+                print "Imported tag: %s" % keyword
         if old_url is not None:
-            redirect, created = Redirect.objects.get_or_create(site=site,
-                old_path=urlparse(old_url).path)
-            redirect.new_path = urlparse(post.get_absolute_url()).path
+            redirect = self.trunc(Redirect, prompt,
+                                  old_path=urlparse(old_url).path,
+                                  new_path=obj.get_absolute_url())
+            Redirect.objects.filter(old_path=redirect["old_path"])
+            redirect, created = Redirect.objects.create(**redirect)
             redirect.save()
+            if created and verbosity >= 1:
+                print "Created redirect for: %s" % redirect.old_path
 
     def handle_import(self, options):
         """
