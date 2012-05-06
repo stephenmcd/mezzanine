027719c3f5388d7c567fc573b332a4b313233fd5
Stephen McDonald <steve@jupo.org>
2012-05-06 10:01:54 +1000

    Proper timezone support for tweets.

--- mezzanine/utils/timezone.py
+++ mezzanine/utils/timezone.py
@@ -1,7 +1,11 @@
 
 # Timezone support with fallback.
 try:
-    from django.utils.timezone import now
+    from django.utils.timezone import (now, get_default_timezone,
+                                       make_aware as django_make_aware)
 except ImportError:
     from datetime import datetime
     now = datetime.now
+    make_aware = lambda v: v
+else:
+    make_aware = lambda v: django_make_aware(v, get_default_timezone())
