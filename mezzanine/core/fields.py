865ae6e901069767e1a48dfade488f5ba790530d
Zachary Gohr <zachary.gohr@gmail.com>
2012-06-14 15:55:48 -0500

    Set the default FileField format to Image so filebrowser can allow for folder selection.

--- mezzanine/core/fields.py
+++ mezzanine/core/fields.py
@@ -70,6 +70,7 @@ else:
         def __init__(self, *args, **kwargs):
             kwargs.setdefault("directory", kwargs.pop("upload_to", None))
             super(FileField, self).__init__(*args, **kwargs)
+            self.format = self.format or 'Image'
 
 
 HtmlField = RichTextField  # For backward compatibility in south migrations.
