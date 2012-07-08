4f264b82a27310cca9d73be8b1b65118a4656ae8
Michał Oleniec <moleniec@gmail.com>
2012-07-09 01:17:19 +0200

    Add customizable profile form.
    - new setting ACCOUNT_PROFILE_FORM
    - add get_profile_form help method
    - add generic form getter into views
    - update mezzanine.account.templatetags

--- mezzanine/accounts/defaults.py
+++ mezzanine/accounts/defaults.py
@@ -37,6 +37,14 @@ register_setting(
 )
 
 register_setting(
+    name="ACCOUNTS_PROFILE_FORM",
+    description=_("Dotted package path and class name of profile form to use "
+        "for account app's views."),
+    editable=False,
+    default='mezzanine.accounts.forms.ProfileForm',
+)
+
+register_setting(
     name="ACCOUNTS_PROFILE_VIEWS_ENABLED",
     description=_("If ``True``, users will have their own public profile "
         "pages."),
