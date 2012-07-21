0f537bf6787e38befa81525fc5fc3773ab983d2f
Stephen McDonald <steve@jupo.org>
2012-07-21 12:30:39 +1000

    Further mimic Django's new project layout.

--- mezzanine/project_template/manage.py
+++ mezzanine/project_template/manage.py
@@ -4,9 +4,12 @@ import os
 import sys
 
 
-# Corrects some pathing issues in various contexts, such as cron jobs.
-from settings import PROJECT_ROOT
+# Corrects some pathing issues in various contexts, such as cron jobs,
+# and the project layout still being in Django 1.3 format.
+from settings import PROJECT_ROOT, PROJECT_DIRNAME
 os.chdir(PROJECT_ROOT)
+sys.path.insert(0, os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
+
 
 # Add the site ID CLI arg to the environment, which allows for the site
 # used in any site related queries to be manually set for management
@@ -16,8 +19,10 @@ for i, arg in enumerate(sys.argv):
         os.environ["MEZZANINE_SITE_ID"] = arg.split("=")[1]
         sys.argv.pop(i)
 
+
 # Run Django.
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    settings_module = "%s.settings" % PROJECT_DIRNAME
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
