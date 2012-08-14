1e740e92582e2ae1b8dc32fc2398b9b5fbffd212
Stephen McDonald <steve@jupo.org>
2012-08-15 07:33:17 +1000

    Fixed auth for password reset.

--- mezzanine/accounts/views.py
+++ mezzanine/accounts/views.py
@@ -153,6 +153,7 @@ def password_reset(request, template="accounts/account_password_reset.html"):
 def password_reset_verify(request, uidb36=None, token=None):
     user = authenticate(uidb36=uidb36, token=token, is_active=True)
     if user is not None:
+        auth_login(request, user)
         return redirect("profile_update")
     else:
         error(request, _("The link you clicked is no longer valid."))
