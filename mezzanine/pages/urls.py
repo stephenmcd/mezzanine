fa56acf0ad8909bf2a16ef9bb3cc07eb93aa9445
Stephen McDonald <steve@jupo.org>
2012-09-26 07:33:31 +1000

    Full support for APPEND_SLASH is False.

--- mezzanine/pages/urls.py
+++ mezzanine/pages/urls.py
@@ -7,5 +7,6 @@ from django.conf import settings
 urlpatterns = patterns("mezzanine.pages.views",
     url("^admin_page_ordering/$", "admin_page_ordering",
         name="admin_page_ordering"),
-    url("^(?P<slug>.*)" + ("/" if settings.APPEND_SLASH else "") + "$", "page", name="page"),
+    url("^(?P<slug>.*)%s$" % ("/" if settings.APPEND_SLASH else ""),
+        "page", name="page"),
 )
