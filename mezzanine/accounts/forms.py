3e32ae3d7ca78b4178f6d291c248ad463df0b515
Renyi Khor <renyi.ace@gmail.com>
2012-10-22 03:09:27 +0800

    Allows LoginForm to be inherited and extended with new fields.

--- mezzanine/accounts/forms.py
+++ mezzanine/accounts/forms.py
@@ -24,7 +24,11 @@ class LoginForm(Html5Mixin, forms.Form):
         Authenticate the given username/email and password. If the fields
         are valid, store the authenticated user for returning via save().
         """
-        self._user = authenticate(**self.cleaned_data)
+        login_data = {
+            "username": self.cleaned_data["username"],
+            "password": self.cleaned_data["password"],
+        }
+        self._user = authenticate(**login_data)
         if self._user is None:
             raise forms.ValidationError(
                              _("Invalid username/email and password"))
