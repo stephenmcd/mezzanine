e3689c39c4dad89a666142ae78e54f1c62a62d21
Stephen McDonald <steve@jupo.org>
2012-10-24 06:56:15 +1100

    pep8 fix / fix missing password exception

--- mezzanine/accounts/forms.py
+++ mezzanine/accounts/forms.py
@@ -24,11 +24,9 @@ class LoginForm(Html5Mixin, forms.Form):
         Authenticate the given username/email and password. If the fields
         are valid, store the authenticated user for returning via save().
         """
-        data = {
-            "username":self.cleaned_data["username"],
-            "password":self.cleaned_data["password"]
-        }
-        self._user = authenticate(**data)
+        username = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+        self._user = authenticate(username=username, password=password)
         if self._user is None:
             raise forms.ValidationError(
                              _("Invalid username/email and password"))
