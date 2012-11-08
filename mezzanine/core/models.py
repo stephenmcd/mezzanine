de86203f32a0d3ed60b13fe55ef7cde71ceb9e36
Eduardo Rivas <e@jerivas.com>
2012-11-08 01:28:55 -0600

    Add previous/next links to blog posts
    Simple implementation of previous/next navigation buttons in the blog detail view. Setting null to False in publish_date is necessary for get_next_by and get_previous_by to work properly (https://docs.djangoproject.com/en/1.4/ref/models/instances/#django.db.models.Model.get_next_by_FOO).

--- mezzanine/core/models.py
+++ mezzanine/core/models.py
@@ -186,7 +186,7 @@ class Displayable(Slugged, MetaData):
             "on the site."))
     publish_date = models.DateTimeField(_("Published from"),
         help_text=_("With Published chosen, won't be shown until this time"),
-        blank=True, null=True)
+        blank=True, null=False)
     expiry_date = models.DateTimeField(_("Expires on"),
         help_text=_("With Published chosen, won't be shown after this time"),
         blank=True, null=True)
