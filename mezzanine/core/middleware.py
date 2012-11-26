81934f67f3dc163762cff7a6c26c6b487987ee7d
Stephen McDonald <steve@jupo.org>
2012-11-26 17:54:13 +1100

    Still log user out for invalid admin access.

--- mezzanine/core/middleware.py
+++ mezzanine/core/middleware.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.http import (HttpResponse, HttpResponseRedirect,
                          HttpResponsePermanentRedirect)
@@ -71,6 +72,7 @@ class SitePermissionMiddleware(object):
             except SitePermission.DoesNotExist:
                 admin_index = reverse("admin:index")
                 if request.path.startswith(admin_index):
+                    logout(request)
                     view_func = admin.site.login
                     extra_context = {"no_site_permission": True}
                     return view_func(request, extra_context=extra_context)
