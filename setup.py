10ae12d60cb8bd9a9a3373f0432fdf57ca3d5e44
Stephen McDonald <steve@jupo.org>
2012-11-30 06:49:55 +1100

    Bump filebrowser safe version to 0.2.13

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ from mezzanine import __version__ as version
 
 install_requires = [
     "django >= 1.3.3",
-    "filebrowser_safe >= 0.2.12",
+    "filebrowser_safe >= 0.2.13",
     "grappelli_safe >= 0.2.11",
     "bleach",
     "pytz",
