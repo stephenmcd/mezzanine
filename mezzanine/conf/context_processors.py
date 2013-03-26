842d0cc389b347045bcc893ba2e4297e74ba9121
dizpers <dizpers@gmail.com>
2013-03-26 11:52:35 +0700

    Fix missing import

--- mezzanine/conf/context_processors.py
+++ mezzanine/conf/context_processors.py
@@ -1,4 +1,3 @@
-
 from mezzanine.utils.cache import (cache_key_prefix, cache_installed,
                                    cache_get, cache_set)
 
@@ -32,12 +31,12 @@ def settings(request):
     """
     Add the settings object to the template context.
     """
+    from mezzanine.conf import settings
     settings_dict = None
     if cache_installed():
         cache_key = cache_key_prefix(request) + "context-settings"
         settings_dict = cache_get(cache_key)
     if not settings_dict:
-        from mezzanine.conf import settings
         settings.use_editable()
         settings_dict = TemplateSettings()
         for k in settings.TEMPLATE_ACCESSIBLE_SETTINGS:
