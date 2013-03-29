ac8253ffd26e9cca43c138c33810e0629befa74c
Stephen McDonald <steve@jupo.org>
2013-03-30 10:03:37 +1100

    Update to latest grappelli/filebrowser-safe.

--- setup.py
+++ setup.py
@@ -26,8 +26,8 @@ from mezzanine import __version__ as version
 
 install_requires = [
     "django >= 1.4, < 1.6",
-    "filebrowser_safe >= 0.2.16",
-    "grappelli_safe >= 0.2.12",
+    "filebrowser_safe >= 0.2.23",
+    "grappelli_safe >= 0.2.15",
     "bleach",
     "pytz",
 ]
