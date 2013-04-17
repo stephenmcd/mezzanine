f92d164c098e372211c442f27e8d37fb245cf21f
Stephen McDonald <steve@jupo.org>
2013-04-18 09:40:27 +1000

    Added default wsgi script to project template.

---
+++ mezzanine/project_template/wsgi.py
@@ -0,0 +1,9 @@
+
+import os
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
