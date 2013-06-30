f6daff55db1299ec9b717b170ef910d53c0b0d96
John Groszko <john@tinythunk.com>
2013-06-30 03:38:19 -0500

    Refactor for file queue

--- mezzanine/galleries/static/mezzanine/js/admin/gallery.js
+++ mezzanine/galleries/static/mezzanine/js/admin/gallery.js
@@ -1,57 +1,98 @@
-$(function() {
-    $('div.form-row.zip_import').remove();
+var ZipImporter = function(selector) {
+    this.element = $(selector);
+    this.browseButton = $('button', selector);
+    this.fileInput = $('input[type="file"]', selector);
+    this.progress = $('.progress');
+    this.progressBar = $('.progress .bar', selector);
+    this.fileQueue = $('.file-queue', selector);
 
-    $('#zip-import-button').click(function (e) {
-	$('#zip-import-file').click();
-	return false;
-    });
+    this.deferredQueue = $.Deferred();
+    this.deferredQueue.resolve();
 
-    $("#zip-import-file").bind('change', function(e) {
-	if(e.currentTarget.files.length > 0)
+    this.browseButton.bind('click', $.proxy(this._handleClick, this));
+    this.fileInput.bind('change', $.proxy(this._handleFileChange, this));
+}
+
+ZipImporter.prototype._handleClick = function(e)
+{
+    this.fileInput.click();
+
+    return false;
+}
+
+ZipImporter.prototype._handleFileChange = function(e)
+{
+    if(e.currentTarget.files.length > 0)
+    {
+	this.progressBar.text("");
+	this.progressBar.removeClass("error");
+	this.progressBar.removeClass("processing");
+	
+	this.progress.fadeIn('fast');
+
+	var dNext = this.deferredQueue;
+	$.each(e.currentTarget.files, $.proxy(function(i, file) {
+	    console.log(file);
+	    this.fileQueue.append("<li>" + file.name + "</li>");
+
+	    dNext = dNext.pipe($.proxy(function() {
+		return this.uploadFile(file);
+	    }, this));
+	}, this));
+
+	/* Prevent these files from being submitted again */
+	this.fileInput.val("");
+    }
+}
+
+ZipImporter.prototype.uploadFile = function(file)
+{
+    var d = $.Deferred();
+
+    this.progressBar.text(file.name);
+    var queueItem = this.fileQueue.children()[0];
+    $(queueItem).detach();
+
+    var xhr = new XMLHttpRequest();
+    xhr.upload.onprogress = $.proxy(function(e) {
+	var done = e.position || e.loaded, total = e.totalSize || e.total;
+	this.progressBar.width((Math.floor(done/total*1000)/10) + "%");
+	if(done == total)
 	{
-	    $('#zip-import-button').attr("disabled", "disabled");
-
-	    $('#zip-import-progress .bar').text("");
-	    $('#zip-import-progress .bar').removeClass("error");
-	    $('#zip-import-progress .bar').removeClass("processing");
-	    $('#zip-import-progress').fadeIn('fast');
-
-	    var file = e.currentTarget.files[0];
-
-	    var xhr = new XMLHttpRequest();
-	    xhr.upload.onprogress = function(e) {
-		var done = e.position || e.loaded, total = e.totalSize || e.total;
-		$("#zip-import-progress .bar").width((Math.floor(done/total*1000)/10) + "%");
-		if(done == total)
-		{
-		    $('#zip-import-progress .bar').text("Processing");
-		    $('#zip-import-progress .bar').addClass("processing");
-		}
-	    };
-
-	    xhr.onreadystatechange = function(e) {
-		if(xhr.readyState == 4)
-		{
-		    if(xhr.status == 200)
-		    {
-			document.location.reload();
-		    }
-		    else
-		    {
-			$('#zip-import-progress .bar').text("Error");
-			$('#zip-import-progress .bar').removeClass("processing");
-			$('#zip-import-progress .bar').addClass("error");
-			$('#zip-import-button').removeAttr("disabled");
-		    }
-		    $('#zip-import-file').val("");
-		}
-	    };
-
-	    var fd = new FormData($('form')[0]);
-	    fd.append('zip_import', file);
-
-	    xhr.open('post', '', true);
-	    xhr.send(fd);
+	    this.progressBar.text("Processing " + file.name);
+	    this.progressBar.addClass("processing");
 	}
-    });
+    }, this);
+
+    xhr.onreadystatechange = $.proxy(function(e) {
+	if(xhr.readyState == 4)
+	{
+	    this.progressBar.removeClass("processing");
+	    if(xhr.status == 200)
+	    {
+		this.progressBar.text("Done");
+		d.resolve();
+	    }
+	    else
+	    {
+		this.progressBar.text("Error on " + file.name);
+		this.progressBar.addClass("error");
+		d.reject();
+	    }
+	}
+    }, this);
+
+
+    var fd = new FormData($('form')[0]);
+    fd.append('zip_import', file);
+
+    xhr.open('post', '', true);
+    xhr.send(fd);
+
+    return d.promise();
+}
+
+$(function() {
+    var zipImporter = new ZipImporter(".form-row.zip-import");
 });
+
