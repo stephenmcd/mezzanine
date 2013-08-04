93cbf8ce8bbdfa3cf4dac056855a68411716eb4d
Thomas Rega <mail@thoreg.org>
2013-08-04 12:10:20 +0200

    Move page related tests into the app directory
    If the app is not installed, the tests are not executed.

---
+++ mezzanine/pages/tests/richtext_page.py
@@ -0,0 +1,407 @@
+from django.contrib.sites.models import Site
+from django.db import connection
+from django.template import Context, Template
+from django.test import TestCase
+from django.utils.html import strip_tags
+
+from mezzanine.conf import settings
+from mezzanine.core.models import CONTENT_STATUS_DRAFT, \
+                                  CONTENT_STATUS_PUBLISHED
+from mezzanine.core.request import current_request
+from mezzanine.generic.models import AssignedKeyword, Keyword
+from mezzanine.pages.models import Page, RichTextPage
+from mezzanine.urls import PAGES_SLUG
+from mezzanine.utils.models import get_user_model
+
+User = get_user_model()
+
+
+class TestsRichTextPage(TestCase):
+    def setUp(self):
+        """
+        Create an admin user.
+        """
+        connection.use_debug_cursor = True
+        self._username = "test"
+        self._password = "test"
+        args = (self._username, "example@example.com", self._password)
+        self._user = User.objects.create_superuser(*args)
+
+    def test_page_ascendants(self):
+        """
+        Test the methods for looking up ascendants efficiently
+        behave as expected.
+        """
+        # Create related pages.
+        primary, created = RichTextPage.objects.get_or_create(title="Primary")
+        secondary, created = primary.children.get_or_create(title="Secondary")
+        tertiary, created = secondary.children.get_or_create(title="Tertiary")
+        # Force a site ID to avoid the site query when measuring queries.
+        setattr(current_request(), "site_id", settings.SITE_ID)
+
+        # Test that get_ascendants() returns the right thing.
+        page = Page.objects.get(id=tertiary.id)
+        ascendants = page.get_ascendants()
+        self.assertEqual(ascendants[0].id, secondary.id)
+        self.assertEqual(ascendants[1].id, primary.id)
+
+        # Test ascendants are returned in order for slug, using
+        # a single DB query.
+        connection.queries = []
+        pages_for_slug = Page.objects.with_ascendants_for_slug(tertiary.slug)
+        self.assertEqual(len(connection.queries), 1)
+        self.assertEqual(pages_for_slug[0].id, tertiary.id)
+        self.assertEqual(pages_for_slug[1].id, secondary.id)
+        self.assertEqual(pages_for_slug[2].id, primary.id)
+
+        # Test page.get_ascendants uses the cached attribute,
+        # without any more queries.
+        connection.queries = []
+        ascendants = pages_for_slug[0].get_ascendants()
+        self.assertEqual(len(connection.queries), 0)
+        self.assertEqual(ascendants[0].id, secondary.id)
+        self.assertEqual(ascendants[1].id, primary.id)
+
+        # Use a custom slug in the page path, and test that
+        # Page.objects.with_ascendants_for_slug fails, but
+        # correctly falls back to recursive queries.
+        secondary.slug += "custom"
+        secondary.save()
+        pages_for_slug = Page.objects.with_ascendants_for_slug(tertiary.slug)
+        self.assertEquals(len(pages_for_slug[0]._ascendants), 0)
+        connection.queries = []
+        ascendants = pages_for_slug[0].get_ascendants()
+        self.assertEqual(len(connection.queries), 2)  # 2 parent queries
+        self.assertEqual(pages_for_slug[0].id, tertiary.id)
+        self.assertEqual(ascendants[0].id, secondary.id)
+        self.assertEqual(ascendants[1].id, primary.id)
+
+    def test_set_parent(self):
+        old_parent, _ = RichTextPage.objects.get_or_create(title="Old parent")
+        new_parent, _ = RichTextPage.objects.get_or_create(title="New parent")
+        child, _ = RichTextPage.objects.get_or_create(title="Child",
+                                                      slug="kid")
+        self.assertTrue(child.parent is None)
+        self.assertTrue(child.slug == "kid")
+
+        child.set_parent(old_parent)
+        child.save()
+        self.assertEqual(child.parent_id, old_parent.id)
+        self.assertTrue(child.slug == "old-parent/kid")
+
+        child = RichTextPage.objects.get(id=child.id)
+        self.assertEqual(child.parent_id, old_parent.id)
+        self.assertTrue(child.slug == "old-parent/kid")
+
+        child.set_parent(new_parent)
+        child.save()
+        self.assertEqual(child.parent_id, new_parent.id)
+        self.assertTrue(child.slug == "new-parent/kid")
+
+        child = RichTextPage.objects.get(id=child.id)
+        self.assertEqual(child.parent_id, new_parent.id)
+        self.assertTrue(child.slug == "new-parent/kid")
+
+        child.set_parent(None)
+        child.save()
+        self.assertTrue(child.parent is None)
+        self.assertTrue(child.slug == "kid")
+
+        child = RichTextPage.objects.get(id=child.id)
+        self.assertTrue(child.parent is None)
+        self.assertTrue(child.slug == "kid")
+
+        child = RichTextPage(title="child2")
+        child.set_parent(new_parent)
+        self.assertEqual(child.slug, "new-parent/child2")
+
+        # Assert that cycles are detected.
+        p1, _ = RichTextPage.objects.get_or_create(title="p1")
+        p2, _ = RichTextPage.objects.get_or_create(title="p2")
+        p2.set_parent(p1)
+        with self.assertRaises(AttributeError):
+            p1.set_parent(p1)
+        with self.assertRaises(AttributeError):
+            p1.set_parent(p2)
+        p2c = RichTextPage.objects.get(title="p2")
+        with self.assertRaises(AttributeError):
+            p1.set_parent(p2c)
+
+    def test_set_slug(self):
+        parent, _ = RichTextPage.objects.get_or_create(title="Parent",
+                                                       slug="parent")
+        child, _ = RichTextPage.objects.get_or_create(title="Child",
+                                                      slug="parent/child",
+                                                      parent_id=parent.id)
+        parent.set_slug("new-parent-slug")
+        parent.save()
+        self.assertTrue(parent.slug == "new-parent-slug")
+
+        parent = RichTextPage.objects.get(id=parent.id)
+        self.assertTrue(parent.slug == "new-parent-slug")
+
+        child = RichTextPage.objects.get(id=child.id)
+        self.assertTrue(child.slug == "new-parent-slug/child")
+
+    def test_description(self):
+        """
+        Test generated description is text version of the first line
+        of content.
+        """
+        description = "<p>How now brown cow</p>"
+        page = RichTextPage.objects.create(title="Draft",
+                                           content=description * 3)
+        self.assertEqual(page.description, strip_tags(description))
+
+    def test_draft_page(self):
+        """
+        Test a draft page as only being viewable by a staff member.
+        """
+        self.client.logout()
+        draft = RichTextPage.objects.create(title="Draft",
+                                            status=CONTENT_STATUS_DRAFT)
+        response = self.client.get(draft.get_absolute_url())
+        self.assertEqual(response.status_code, 404)
+        self.client.login(username=self._username, password=self._password)
+        response = self.client.get(draft.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+    def queries_used_for_template(self, template, **context):
+        """
+        Return the number of queries used when rendering a template
+        string.
+        """
+        connection.queries = []
+        t = Template(template)
+        t.render(Context(context))
+        return len(connection.queries)
+
+    def create_recursive_objects(self, model, parent_field, **kwargs):
+        """
+        Create multiple levels of recursive objects.
+        """
+        per_level = range(3)
+        for _ in per_level:
+            kwargs[parent_field] = None
+            level1 = model.objects.create(**kwargs)
+            for _ in per_level:
+                kwargs[parent_field] = level1
+                level2 = model.objects.create(**kwargs)
+                for _ in per_level:
+                    kwargs[parent_field] = level2
+                    model.objects.create(**kwargs)
+
+    def test_page_menu_queries(self):
+        """
+        Test that rendering a page menu executes the same number of
+        queries regardless of the number of pages or levels of
+        children.
+        """
+        template = ('{% load pages_tags %}'
+                    '{% page_menu "pages/menus/tree.html" %}')
+        before = self.queries_used_for_template(template)
+        self.assertTrue(before > 0)
+        self.create_recursive_objects(RichTextPage, "parent", title="Page",
+                                      status=CONTENT_STATUS_PUBLISHED)
+        after = self.queries_used_for_template(template)
+        self.assertEquals(before, after)
+
+    def test_page_menu_flags(self):
+        """
+        Test that pages only appear in the menu templates they've been
+        assigned to show in.
+        """
+        menus = []
+        pages = []
+        template = "{% load pages_tags %}"
+        for i, label, path in settings.PAGE_MENU_TEMPLATES:
+            menus.append(i)
+            pages.append(RichTextPage.objects.create(in_menus=list(menus),
+                title="Page for %s" % unicode(label),
+                status=CONTENT_STATUS_PUBLISHED))
+            template += "{%% page_menu '%s' %%}" % path
+        rendered = Template(template).render(Context({}))
+        for page in pages:
+            self.assertEquals(rendered.count(page.title), len(page.in_menus))
+
+    def test_page_menu_default(self):
+        """
+        Test that the default value for the ``in_menus`` field is used
+        and that it doesn't get forced to unicode.
+        """
+        old_menu_temp = settings.PAGE_MENU_TEMPLATES
+        old_menu_temp_def = settings.PAGE_MENU_TEMPLATES_DEFAULT
+        try:
+            # MenusField initializes choices and default during model
+            # loading, so we can't just override settings.
+            from mezzanine.pages.models import BasePage
+            from mezzanine.pages.fields import MenusField
+            settings.PAGE_MENU_TEMPLATES = ((8, 'a', 'a'), (9, 'b', 'b'))
+
+            settings.PAGE_MENU_TEMPLATES_DEFAULT = None
+
+            class P1(BasePage):
+                in_menus = MenusField(blank=True, null=True)
+            self.assertEqual(P1().in_menus[0], 8)
+
+            settings.PAGE_MENU_TEMPLATES_DEFAULT = tuple()
+
+            class P2(BasePage):
+                in_menus = MenusField(blank=True, null=True)
+            self.assertEqual(P2().in_menus, None)
+
+            settings.PAGE_MENU_TEMPLATES_DEFAULT = [9]
+
+            class P3(BasePage):
+                in_menus = MenusField(blank=True, null=True)
+            self.assertEqual(P3().in_menus[0], 9)
+        finally:
+            settings.PAGE_MENU_TEMPLATES = old_menu_temp
+            settings.PAGE_MENU_TEMPLATES_DEFAULT = old_menu_temp_def
+
+    def test_keywords(self):
+        """
+        Test that the keywords_string field is correctly populated.
+        """
+        page = RichTextPage.objects.create(title="test keywords")
+        keywords = set(["how", "now", "brown", "cow"])
+        Keyword.objects.all().delete()
+        for keyword in keywords:
+            keyword_id = Keyword.objects.get_or_create(title=keyword)[0].id
+            page.keywords.add(AssignedKeyword(keyword_id=keyword_id))
+        page = RichTextPage.objects.get(id=page.id)
+        self.assertEquals(keywords, set(page.keywords_string.split()))
+        # Test removal.
+        first = Keyword.objects.all()[0]
+        keywords.remove(first.title)
+        first.delete()
+        page = RichTextPage.objects.get(id=page.id)
+        self.assertEquals(keywords, set(page.keywords_string.split()))
+        page.delete()
+
+    def test_search(self):
+        """
+        Pages with status "Draft" should not be listed in the search result.
+        """
+        RichTextPage.objects.all().delete()
+        published = {"status": CONTENT_STATUS_PUBLISHED}
+        first = RichTextPage.objects.create(title="test page",
+                                           status=CONTENT_STATUS_DRAFT).id
+        second = RichTextPage.objects.create(title="test another test page",
+                                            **published).id
+        # Draft shouldn't be a result.
+        results = RichTextPage.objects.search("test")
+        self.assertEqual(len(results), 1)
+        RichTextPage.objects.filter(id=first).update(**published)
+        results = RichTextPage.objects.search("test")
+        self.assertEqual(len(results), 2)
+        # Either word.
+        results = RichTextPage.objects.search("another test")
+        self.assertEqual(len(results), 2)
+        # Must include first word.
+        results = RichTextPage.objects.search("+another test")
+        self.assertEqual(len(results), 1)
+        # Mustn't include first word.
+        results = RichTextPage.objects.search("-another test")
+        self.assertEqual(len(results), 1)
+        if results:
+            self.assertEqual(results[0].id, first)
+        # Exact phrase.
+        results = RichTextPage.objects.search('"another test"')
+        self.assertEqual(len(results), 1)
+        if results:
+            self.assertEqual(results[0].id, second)
+        # Test ordering.
+        results = RichTextPage.objects.search("test")
+        self.assertEqual(len(results), 2)
+        if results:
+            self.assertEqual(results[0].id, second)
+
+    def _create_page(self, title, status):
+        return RichTextPage.objects.create(title=title, status=status)
+
+    def _test_site_pages(self, title, status, count):
+        # test _default_manager
+        pages = RichTextPage._default_manager.all()
+        self.assertEqual(pages.count(), count)
+        self.assertTrue(title in [page.title for page in pages])
+
+        # test objects manager
+        pages = RichTextPage.objects.all()
+        self.assertEqual(pages.count(), count)
+        self.assertTrue(title in [page.title for page in pages])
+
+        # test response status code
+        code = 200 if status == CONTENT_STATUS_PUBLISHED else 404
+        pages = RichTextPage.objects.filter(status=status)
+        response = self.client.get(pages[0].get_absolute_url())
+        self.assertEqual(response.status_code, code)
+
+    def test_mulisite(self):
+        from django.conf import settings
+
+        # setup
+        try:
+            old_site_id = settings.SITE_ID
+        except:
+            old_site_id = None
+
+        site1 = Site.objects.create(domain="site1.com")
+        site2 = Site.objects.create(domain="site2.com")
+
+        # create pages under site1, which should be only accessible
+        # when SITE_ID is site1
+        settings.SITE_ID = site1.pk
+        site1_page = self._create_page("Site1", CONTENT_STATUS_PUBLISHED)
+        self._test_site_pages("Site1", CONTENT_STATUS_PUBLISHED, count=1)
+
+        # create pages under site2, which should only be accessible
+        # when SITE_ID is site2
+        settings.SITE_ID = site2.pk
+        self._create_page("Site2", CONTENT_STATUS_PUBLISHED)
+        self._test_site_pages("Site2", CONTENT_STATUS_PUBLISHED, count=1)
+
+        # original page should 404
+        response = self.client.get(site1_page.get_absolute_url())
+        self.assertEqual(response.status_code, 404)
+
+        # change back to site1, and only the site1 pages should be retrieved
+        settings.SITE_ID = site1.pk
+        self._test_site_pages("Site1", CONTENT_STATUS_PUBLISHED, count=1)
+
+        # insert a new record, see the count change
+        self._create_page("Site1 Draft", CONTENT_STATUS_DRAFT)
+        self._test_site_pages("Site1 Draft", CONTENT_STATUS_DRAFT, count=2)
+        self._test_site_pages("Site1 Draft", CONTENT_STATUS_PUBLISHED, count=2)
+
+        # change back to site2, and only the site2 pages should be retrieved
+        settings.SITE_ID = site2.pk
+        self._test_site_pages("Site2", CONTENT_STATUS_PUBLISHED, count=1)
+
+        # insert a new record, see the count change
+        self._create_page("Site2 Draft", CONTENT_STATUS_DRAFT)
+        self._test_site_pages("Site2 Draft", CONTENT_STATUS_DRAFT, count=2)
+        self._test_site_pages("Site2 Draft", CONTENT_STATUS_PUBLISHED, count=2)
+
+        # tear down
+        if old_site_id:
+            settings.SITE_ID = old_site_id
+        else:
+            del settings.SITE_ID
+
+        site1.delete()
+        site2.delete()
+
+    def test_overridden_page(self):
+        """
+        Test that a page with a slug matching a non-page urlpattern
+        return ``True`` for its overridden property.
+        """
+        # BLOG_SLUG is empty then urlpatterns for pages are prefixed
+        # with PAGE_SLUG, and generally won't be overridden. In this
+        # case, there aren't any overridding URLs by default, so bail
+        # on the test.
+        if PAGES_SLUG:
+            return
+        page, created = RichTextPage.objects.get_or_create(slug="edit")
+        self.assertTrue(page.overridden())
