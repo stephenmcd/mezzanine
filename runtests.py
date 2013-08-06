2cc94d232480fb6ee3338c4015e38b5542fe8cf9
Stephen McDonald <steve@jupo.org>
2013-08-07 09:09:14 +1000

    Inject all Mezzanine apps into the temp settings module when Mezzanine itself is being tested.

--- runtests.py
+++ runtests.py
@@ -12,6 +12,32 @@ def runtests():
     local_settings_path = os.path.join(project_path, "local_settings.py")
     if not os.path.exists(local_settings_path):
         shutil.copy(local_settings_path + ".template", local_settings_path)
+        with open(local_settings_path, "a") as f:
+            f.write("""
+
+INSTALLED_APPS = (
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.redirects",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
+    "django.contrib.staticfiles",
+    "mezzanine.boot",
+    "mezzanine.conf",
+    "mezzanine.core",
+    "mezzanine.generic",
+    "mezzanine.blog",
+    "mezzanine.forms",
+    "mezzanine.pages",
+    "mezzanine.galleries",
+    "mezzanine.twitter",
+    "mezzanine.accounts",
+    "mezzanine.mobile",
+)
+
+                """)
         atexit.register(lambda: os.remove(local_settings_path))
 
     from django.core.management.commands import test
