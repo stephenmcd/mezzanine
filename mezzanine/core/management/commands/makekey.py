0f0594f6de34c8b24a2b2bca52119131d41e4ebb
Alyssa <cojacfar@gmail.com>
2013-10-30 09:26:15 -0700

    Updated makekey.py to be functional. Need to test still.

--- mezzanine/core/management/commands/makekey.py
+++ mezzanine/core/management/commands/makekey.py
@@ -1,9 +1,26 @@
 
 import os
+from optparse import make_option
+
+from django.conf import settings 
+from django.core.management.base import BaseCommand, CommandError
 
 from uuid import uuid4
-# Generate a unique SECRET_KEY for the project's local_setttings module.
-    #settings_path = os.path.join(os.getcwd(), project_name, "local_settings.py")
+
+class Command(NoArgsCommand):
+    '''
+    Used to generate a new SECRET_KEY and NEVERCACHE_KEY
+    into local_settings.py. Run this command after
+    deployment so that your new local_settings.py gets
+    updated
+    '''
+    help = ("Creates new SECRET_KEY and NEVERCACHE_KEY into local_settings.py.")
+    can_import_settings = True
+    option_list = BaseCommand.option_list
+
+    
+    def handle_noargs(self, **options):
+    # Generate a unique SECRET_KEY for the project's local_setttings module.
     settings_path = os.getcwd()
     with open(settings_path, "r") as f:
         data = f.read()
