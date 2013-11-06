543470e59e26d4f9fb0b837aa967bf869b2bb9f9
Ed Schofield <ed@pythoncharmers.com>
2013-11-06 13:19:56 +1100

    Add absolute_import to prevent implicit relative import of html.py on Py2

--- mezzanine/utils/html.py
+++ mezzanine/utils/html.py
@@ -1,4 +1,4 @@
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
 from future import standard_library
 from future.builtins import chr
 from future.builtins import int
