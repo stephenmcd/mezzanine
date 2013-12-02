a87cda5bcd85d44cc7272e3e4019324fa509017b
Stephen McDonald <steve@jupo.org>
2013-12-03 07:34:32 +1100

    Allow None to be used as a per_page arg for pagination, to bypass pagination altogther.

--- mezzanine/utils/views.py
+++ mezzanine/utils/views.py
@@ -130,6 +130,8 @@ def paginate(objects, page_num, per_page, max_paging_links):
     Return a paginated page for the given objects, giving it a custom
     ``visible_page_range`` attribute calculated from ``max_paging_links``.
     """
+    if not per_page:
+        return Paginator(objects, 0)
     paginator = Paginator(objects, per_page)
     try:
         page_num = int(page_num)
