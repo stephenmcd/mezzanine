e9599c048d18189ce223da46f39a98179a542180
Stephen McDonald <steve@jupo.org>
2013-12-10 10:36:10 +1100

    jQuery namespace cleanup

--- mezzanine/core/static/mezzanine/js/admin/collapse_backport.js
+++ mezzanine/core/static/mezzanine/js/admin/collapse_backport.js
@@ -1,5 +1,5 @@
-jQuery(document).ready(function($){
-     
+jQuery(function($) {
+
     /// FIELDSETS
     $('fieldset[class*="collapse-closed"]').each(function() {
         $(this).addClass("collapsed");
