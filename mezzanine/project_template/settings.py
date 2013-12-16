141cdf2c4148a7e45035c9cea8bf3aabfaa3da0e
Sebastian Clemens <mail@sebastian-clemens.de>
2013-12-16 21:24:59 +0100

    Added django.middleware.locale.LocaleMiddleware to MIDDLEWARE_CLASSES

--- mezzanine/project_template/settings.py
+++ mezzanine/project_template/settings.py
@@ -272,6 +272,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 MIDDLEWARE_CLASSES = (
     "mezzanine.core.middleware.UpdateCacheMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
