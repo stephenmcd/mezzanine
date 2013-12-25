58b71057f8e3493ef274eb09d475026ef2864405
Stephen McDonald <steve@jupo.org>
2013-12-26 08:08:26 +1100

    Bump grappelli version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from mezzanine import __version__ as version
 install_requires = [
     "django >= 1.4.10, != 1.6.0, < 1.7",
     "filebrowser_safe >= 0.3.1",
-    "grappelli_safe >= 0.3.1",
+    "grappelli_safe >= 0.3.3",
     "html5lib == 0.95",
     "pytz >= 2013b",
     "requests == 1.2.3",
