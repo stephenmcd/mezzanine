14d1d52ee5737e4f0fab04a29edf0ca3a0a2ba81
Stephen McDonald <steve@jupo.org>
2014-02-11 12:54:11 +1100

    Refactor the live-editing toolbar: style tweaks, default toolbar to closed, logout form now a link that uses mezzanine.accounts logout if present (which shows a logout message).

--- mezzanine/core/static/mezzanine/js/editable.js
+++ mezzanine/core/static/mezzanine/js/editable.js
@@ -1,22 +1,28 @@
 jQuery(function($) {
 
+    $(window.__toolbar_html).appendTo('body');
+
+    var iconOpen = '&#8677;', iconClose = '&#8676;';
+    var cookieName = 'mezzanine-admin-toolbar';
+    var cookieValue = '1';
+    var toolbar = $('#editable-toolbar');
+    var toolbarToggleButton = $('#editable-toolbar-toggle');
+    var toolbarElements = toolbar.find('*[id!=editable-toolbar-toggle]');
+    var links = $('.editable-link');
+
     // Add AJAX submit handler for each editable form.
     $('.editable-form').submit(function() {
         var form = $(this);
         var loading = $('#editable-loading');
+        var win = $(window);
         var showError = function(msg) {
-            if (msg) {
-                msg = ': ' + msg;
-            } else {
-                msg = '';
-            }
-            alert('An error occurred' + msg);
+            alert('An error occurred' + (msg ? ': ' + msg : ''));
             loading.hide();
             form.show();
         };
         loading.css({
-            top: ($(window).height() - loading.height()) / 2 + $(window).scrollTop() + "px",
-            left: ($(window).width() - loading.width()) / 2 + $(window).scrollLeft() + "px"
+            top: (win.height() - loading.height()) / 2 + win.scrollTop() + "px",
+            left: (win.width() - loading.width()) / 2 + win.scrollLeft() + "px"
         });
         form.hide();
         loading.show();
@@ -35,76 +41,72 @@ jQuery(function($) {
         return false;
     })
 
-    // Iterate through each of the editable areas and set them up.
-    var realign = function() {
+    // Iterate through each of the editable areas and set them up:
+    //  - position the editable area's edit link
+    //  - apply the editable area's overlay handler
+    //  - position the editable area's highlight
+    var alignEditButtons = function() {
         $.each($('.editable-original'), function(i, editable) {
             editable = $(editable);
-            // Position the editable area's edit link.
             var link = editable.next('.editable-link');
-            link.offset({top: editable.offset().top,
-                left: editable.offset().left - link.outerWidth() - 1});
-            // Apply the editable area's overlay handler.
-            var expose = {color: '#333', loadSpeed: 200, opacity: 0.9};
-            var overlay = {expose: expose, closeOnClick: true, close: ':button', left: 'center', top: 'center'};
-            link.overlay(overlay);
-            // Position the editable area's highlight.
-            var highlight = link.next('.editable-highlight')
-            highlight.css({
+            link.offset({
+                top: editable.offset().top,
+                left: editable.offset().left - link.outerWidth() - 5
+            }).overlay({
+                expose: {color: '#333', loadSpeed: 200, opacity: 0.9},
+                closeOnClick: true, close: ':button',
+                left: 'center', top: 'center'
+            });
+            link.next('.editable-highlight').css({
                 width: editable.width(),
                 height: editable.height()
+            }).offset({
+                top: editable.offset().top, left: editable.offset().left
             });
-            highlight.offset({top: editable.offset().top, left: editable.offset().left});
         });
     };
 
-    realign();
+    alignEditButtons();
+    $('body, .editable-original').on('resize', alignEditButtons);
 
     // Show/hide the editable area's highlight when mousing over/out the of
     // the edit link.
-    $('.editable-link').hover(function(e) {
+    links.hover(function(e) {
         $(this).next('.editable-highlight').css('visibility', 'visible');
     }, function(e) {
         $(this).next('.editable-highlight').css('visibility', 'hidden');
     });
 
-    $('body, .editable-original').on('resize', function(e) {
-        realign();
-    });
-
-    // Add the toolbar HTML and handlers.
-    var cookie = 'mezzanine-admin-toolbar';
-    var at = ('; ' + document.cookie).indexOf('; ' + cookie + '=');
-    var closed = false;
-    if (at > -1) {
-        closed = document.cookie.substr(at +
-            cookie.length + 1).split(';')[0];
-    }
-    $(window.__toolbar_html).appendTo('body');
-    $('#editable-toolbar-toggle').click(function() {
-        var toggle = $(this);
-        var links = $('.editable-link');
-        var toolbar = $('#editable-toolbar *[id!=editable-toolbar-toggle]');
-        if (toggle.text() == '<<') {
-            toggle.text('>>');
-            toolbar.hide();
-            links.css('visibility', 'hidden');
-            document.cookie = cookie + '=1; path=/';
-        } else {
-            toggle.text('<<');
-            toolbar.show();
-            links.css('visibility', 'visible');
-            document.cookie = cookie + '=; path=/';
+    // Get the toolbar state from cookie, returns null if never set.
+    var toolbarOpen = function() {
+        var at = ('; ' + document.cookie).indexOf('; ' + cookieName + '=');
+        if (at > -1) {
+            at += cookieName.length + 1
+            return document.cookie.substr(at).split(';')[0] == cookieValue;
         }
-        return false;
-    });
-    $('#editable-toolbar-logout').click(function() {
-        $('#editable-toolbar').submit();
-        return false;
-    });
+        return null;
+    };
 
-    $('#editable-toolbar').fadeIn('slow');
-    if (!closed) {
-        $('#editable-toolbar-toggle').click();
-    }
+    // Shows/hides the toolbar, also setting the cookie state.
+    var toggleToolbar = function(hide) {
+        if (hide) {
+            toolbarToggleButton.html(iconOpen);
+            toolbarElements.hide();
+            links.css('visibility', 'hidden');
+            document.cookie = cookieName + '=' + cookieValue + '; path=/';
+        } else {
+            toolbarToggleButton.html(iconClose);
+            toolbarElements.show();
+            links.css('visibility', 'visible');
+            document.cookie = cookieName + '=; path=/';
+        }
+    };
+
+    toolbar.fadeIn('slow');
+    toggleToolbar(toolbarOpen() != false);  // null first time, start closed.
+    toolbarToggleButton.click(function() {
+        toggleToolbar(!toolbarOpen());
+        return false;
+    });
 
 });
