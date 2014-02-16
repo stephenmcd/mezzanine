0dfb2e20bf792a2a706b878151b62fca4ff31b1a
Stephen McDonald <steve@jupo.org>
2014-02-17 08:25:40 +1100

    Bump grappelli version to 0.3.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ try:
         install_requires=[
             "django >= 1.4.10, != 1.6.0, < 1.7",
             "filebrowser_safe >= 0.3.2",
-            "grappelli_safe >= 0.3.4",
+            "grappelli_safe >= 0.3.6",
             "tzlocal == 1.0",
             "bleach >= 1.4",
             "requests >= 2.1.0",
