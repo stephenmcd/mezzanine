1c7da1e0d975ad298a3b1c4c432114a5e73081ac
Stephen McDonald <steve@jupo.org>
2014-03-12 08:43:34 +1100

    Fix device handling for non-ascii user agents. Closes #976.

--- mezzanine/utils/device.py
+++ mezzanine/utils/device.py
@@ -21,6 +21,10 @@ def device_from_request(request):
         except KeyError:
             pass
         else:
+            try:
+                user_agent = user_agent.decode("utf-8")
+            except AttributeError:
+                pass
             for (device, ua_strings) in settings.DEVICE_USER_AGENTS:
                 for ua_string in ua_strings:
                     if ua_string.lower() in user_agent:
