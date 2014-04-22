f945daf05769228f77c8efd0d312bb7d2d5b9eae
Stephen McDonald <steve@jupo.org>
2014-04-22 17:11:19 +1000

    Widget js shouldn't need staticfiles handling

--- mezzanine/core/forms.py
+++ mezzanine/core/forms.py
@@ -7,7 +7,6 @@ from django import forms
 from django.forms.extras.widgets import SelectDateWidget
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
-from django.contrib.staticfiles.storage import staticfiles_storage
 
 from mezzanine.conf import settings
 from mezzanine.core.models import Orderable
@@ -39,8 +38,9 @@ class Html5Mixin(object):
 
 _tinymce_js = ()
 if settings.GRAPPELLI_INSTALLED:
-    _path = "grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js"
-    _tinymce_js = (staticfiles_storage.url(_path), settings.TINYMCE_SETUP_JS)
+    _path =
+    _tinymce_js = ("grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js",
+                   settings.TINYMCE_SETUP_JS)
 
 
 class TinyMceWidget(forms.Textarea):
