771cb7c784642a739078a077c442bb6874146b36
Pavan Rikhi <pavan.rikhi+agit@gmail.com>
2014-04-24 03:23:17 -0400

    Decouple the Blog Post List from RichTextPages
    The blog_post_list.html template currently requires that any `page`
    context variable passed to it has a `richtextpage` attribute.
    
    These changes modify the template, making the `richtextpage` attribute
    optional. This allows users to create other Page types pointing to the
    URL of the Blog Post List.

--- mezzanine/blog/tests.py
+++ mezzanine/blog/tests.py
@@ -12,7 +12,7 @@ from mezzanine.blog.models import BlogPost
 from mezzanine.conf import settings
 
 from mezzanine.core.models import CONTENT_STATUS_PUBLISHED
-from mezzanine.pages.models import RichTextPage
+from mezzanine.pages.models import Page, RichTextPage
 from mezzanine.utils.tests import TestCase
 
 
@@ -49,3 +49,10 @@ class BlogTests(TestCase):
         self.assertTrue(len(response.redirect_chain) > 0)
         redirect_path = urlparse(response.redirect_chain[0][0]).path
         self.assertEqual(redirect_path, settings.LOGIN_URL)
+
+    def test_blog_post_list_can_use_any_page_type(self):
+        """Test that the blog post list can use any Page type."""
+        slug = settings.BLOG_SLUG or "/"
+        Page.objects.create(title="blog", slug=slug)
+        response = self.client.get(reverse("blog_post_list"))
+        self.assertEqual(response.status_code, 200)
