56fd1025f1ee2485765ff6c5fdef8ed1f4e20956
Stephen McDonald <steve@jupo.org>
2014-06-07 20:21:32 +1000

    Bump grappelli/filebrowser versions.

--- setup.py
+++ setup.py
@@ -51,8 +51,8 @@ try:
         packages=find_packages(),
         install_requires=[
             "django >= 1.4.10, != 1.6.0, < 1.7",
-            "filebrowser_safe >= 0.3.3",
-            "grappelli_safe >= 0.3.10",
+            "filebrowser_safe >= 0.3.4",
+            "grappelli_safe >= 0.3.11",
             "tzlocal == 1.0",
             "bleach >= 1.4",
             "requests >= 2.1.0",
