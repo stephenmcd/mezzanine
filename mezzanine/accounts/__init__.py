6d12323e165278f80144fa725c3ab69454efdc98
Alex Hill <alex@hill.net.au>
2014-06-18 10:40:20 +0800

    Use ProfileNotConfigured in profile functions

--- mezzanine/accounts/__init__.py
+++ mezzanine/accounts/__init__.py
@@ -14,52 +14,44 @@ from mezzanine.utils.models import get_user_model, get_model
 from mezzanine.utils.importing import import_dotted_path
 
 
+class ProfileNotConfigured(Exception):
+    pass
+
+
 def get_profile_model():
     """
-    Returns the profile model defined by the ``AUTH_PROFILE_MODULE``
-    setting, or ``None`` if no profile model is defined.
+    Returns the Mezzanine profile model, defined in
+    settings.AUTH_PROFILE_MODULE, or None if no profile model is configured.
     """
-    profile_model_name = getattr(settings, "AUTH_PROFILE_MODULE", None)
 
-    if not profile_model_name:
-        return None
+    if not getattr(settings, "AUTH_PROFILE_MODULE", None):
+        raise ProfileNotConfigured
 
     try:
-        app_label, model_name = profile_model_name.split(".")
-        try:
-            profile_model = get_model(app_label, model_name)
-        except LookupError:
-            raise ImproperlyConfigured("No model corresponding to the value "
-                                       "of AUTH_PROFILE_MODULE was found.")
-        else:
-            return profile_model
-    except (ValueError, AttributeError):
-        raise ImproperlyConfigured("Value for AUTH_PROFILE_MODULE must "
-                                   "be an \"app_label.ModelName\" string.")
+        return get_model(settings.AUTH_PROFILE_MODULE)
+    except ValueError:
+        raise ImproperlyConfigured("AUTH_PROFILE_MODULE must be of the form 'app_label.model_name'")
+    except LookupError:
+        raise ImproperlyConfigured("AUTH_PROFILE_MODULE refers to model '%s' that has not been installed" % settings.AUTH_PROFILE_MODULE)
 
 
 def get_profile_for_user(user):
     """
     Returns site-specific profile for this user. Raises
-    SiteProfileNotAvailable if this site does not allow profiles.
+    ProfileNotConfigured if AUTH_PROFILE_MODULE is not set,
+    and ImproperlyConfigured if the corresponding model can't be found.
     """
     if not hasattr(user, '_mezzanine_profile'):
-        from django.conf import settings
-        if not getattr(settings, 'AUTH_PROFILE_MODULE', False):
-            raise ImproperlyConfigured(
-                'You need to set AUTH_PROFILE_MODULE in your project '
-                'settings')
-
+        # Raises ProfileNotConfigured if not bool(AUTH_PROFILE_MODULE)
         profile_model = get_profile_model()
-        if not profile_model:
-            return None
+        profile_manager = profile_model._default_manager.using(user._state.db)
 
-        try:
-            user._mezzanine_profile = profile_model._default_manager.using(
-                user._state.db).get(user__id__exact=user.id)
-            user._mezzanine_profile.user = user
-        except (ImportError, ImproperlyConfigured):
-            raise ImproperlyConfigured
+        user_field = get_profile_user_fieldname(profile_model, user.__class__)
+        profile, created = profile_manager.get_or_create(**{user_field: user})
+
+        profile.user = user
+        user._mezzanine_profile = profile
+
     return user._mezzanine_profile
 
 
@@ -83,10 +75,9 @@ def get_profile_user_fieldname(profile_model=None, user_model=None):
     """
     Profile = profile_model or get_profile_model()
     User = user_model or get_user_model()
-    if Profile is not None:
-        for field in Profile._meta.fields:
-            if field.rel and field.rel.to == User:
-                return field.name
+    for field in Profile._meta.fields:
+        if field.rel and field.rel.to == User:
+            return field.name
     raise ImproperlyConfigured("Value for AUTH_PROFILE_MODULE does not "
                                "contain a ForeignKey field for auth.User: %s"
                                % Profile.__name__)
