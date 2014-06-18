2a343aa13fb000ffb56173e77207fe14c3bbe85c
Alex Hill <alex@hill.net.au>
2014-06-18 10:39:11 +0800

    Use get_profile_for_user() in profile signal handler

--- mezzanine/accounts/models.py
+++ mezzanine/accounts/models.py
@@ -1,6 +1,6 @@
 from django.db import DatabaseError, connection
 from django.db.models.signals import post_save
-from mezzanine.accounts import get_profile_user_fieldname
+from mezzanine.accounts import get_profile_user_fieldname, get_profile_for_user
 from mezzanine.conf import settings
 from mezzanine.utils.models import lazy_model_ops
 
@@ -16,11 +16,10 @@ if getattr(settings, "AUTH_PROFILE_MODULE", None):
 
         user_field = get_profile_user_fieldname(profile_model, user_model)
 
-        def create_profile(sender, instance, created, **kwargs):
+        def create_profile(sender, instance, created, **_):
             if created:
                 try:
-                    kwargs = {str(user_field): instance}
-                    profile_model.objects.create(**kwargs)
+                    get_profile_for_user(instance)
                 except DatabaseError:
                     # User creation in initial syncdb may have been triggered,
                     # while profile model is under migration management and
