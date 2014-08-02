ea8d0d5e4af2a86b48ce794eb7d64360667f23d1
Stephen McDonald <steve@jupo.org>
2014-08-02 14:37:34 +1000

    Remove working directory hack from bundled manage.py - conflicts with Django runserver's auto-reloader when run from outside of project root.

--- mezzanine/project_template/manage.py
+++ mezzanine/project_template/manage.py
@@ -8,7 +8,6 @@ import sys
 # Corrects some pathing issues in various contexts, such as cron jobs,
 # and the project layout still being in Django 1.3 format.
 from settings import PROJECT_ROOT, PROJECT_DIRNAME
-os.chdir(PROJECT_ROOT)
 sys.path.append(os.path.abspath(os.path.join(PROJECT_ROOT, "..")))
 
 
