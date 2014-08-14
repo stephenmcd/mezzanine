915fcfe8a66d8ee3d3f45d701ed7aa5a3b7acf26
Stephen McDonald <steve@jupo.org>
2014-08-14 17:24:03 +1000

    Fix dynamic INSTALLED_APPS in setuptools test runner.

--- mezzanine/bin/runtests.py
+++ mezzanine/bin/runtests.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+import multiprocessing as mp
 import atexit
 import os
 import shutil
@@ -24,12 +25,39 @@ def main(package="mezzanine"):
 
     if not os.path.exists(local_settings_path):
         shutil.copy(local_settings_path + ".template", local_settings_path)
-        from django.conf import settings
-        apps = list(settings.INSTALLED_APPS)
-        if "mezzanine.accounts" not in apps:
-            apps += ["mezzanine.accounts"]
-        with open(local_settings_path, "a") as f:
-            f.write("INSTALLED_APPS = %s" % apps)
+
+        # What we want to do is add some INSTALLED_APPS to settings
+        # when testing. We do this by copying over a local_settings.py
+        # module, then deleting that when we're done via atexit.
+        #
+        # Now that's *kinda* hacky, but the fun hasn't started yet. In
+        # order to get the current list of INSTALLED_APPS so that we
+        # can append to it without having to hard code it entirely,
+        # we need to read the *current* value of INSTALLED_APPS, which
+        # means we need to import it - but by doing so, our
+        # local_settings.py improt will be triggered, and we won't have
+        # the chance to import the one we're about to write - so we do
+        # the settings import/read and local_settings file write in
+        # a separate process, which means by the time all the settings
+        # importing *first* happens in the foreground process, the
+        # local_settings.py file will have be written.
+        #
+        # This is up there in my worst hacks of all time. ¯\_(ツ)_/¯
+        #
+        # PS: We need to import the top-level multiprocessing module
+        # otherwise we hit: http://bugs.python.org/issue15881#msg170215
+
+        def set_apps(local_settings_path):
+            from django.conf import settings
+            apps = list(settings.INSTALLED_APPS)
+            if "mezzanine.accounts" not in apps:
+                apps += ["mezzanine.accounts"]
+            with open(local_settings_path, "a") as f:
+                f.write("INSTALLED_APPS = %s" % apps)
+
+        proc = mp.Process(target=set_apps, args=(local_settings_path,))
+        proc.start()
+        proc.join()
         atexit.register(lambda: os.remove(local_settings_path))
 
     from django.core.management.commands import test
