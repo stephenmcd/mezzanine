e13ca018799dcdb7d90dced0d92763fa2e1e4e5a
Stephen McDonald <steve@jupo.org>
2014-08-28 22:22:59 +1000

    Fix local_settings import error check for Python 3.

--- mezzanine/project_template/settings.py
+++ mezzanine/project_template/settings.py
@@ -343,7 +343,7 @@ OPTIONAL_APPS = (
 try:
     from local_settings import *
 except ImportError as e:
-    if "local_settings" not in e.message:
+    if "local_settings" not in str(e):
         raise e
 
 
