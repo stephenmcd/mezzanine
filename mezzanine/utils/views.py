2ff9683331e8ffcb8fbd0f076b4937ad65be263d
wrwrwr <git@wr.waw.pl>
2014-10-16 11:15:34 +0200

    Replaced usage of Options.get_(add|change|delete)_permission with auth.get_permission_codename. Just a single case in utils/views.py.
    The former one is deprecated and will be removed in Django 1.8; see
    https://code.djangoproject.com/ticket/20642.

--- mezzanine/utils/views.py
+++ mezzanine/utils/views.py
@@ -13,6 +13,7 @@ except ImportError:     # Python 2
     from urllib2 import Request, urlopen
 
 import django
+from django.contrib.auth import get_permission_codename
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.forms import EmailField, URLField, Textarea
 from django.template import RequestContext
@@ -35,7 +36,8 @@ def is_editable(obj, request):
     if hasattr(obj, "is_editable"):
         return obj.is_editable(request)
     else:
-        perm = obj._meta.app_label + "." + obj._meta.get_change_permission()
+        codename = get_permission_codename("change", obj._meta)
+        perm = "%s.%s" % (obj._meta.app_label, codename)
         return (request.user.is_authenticated() and
                 has_site_permission(request.user) and
                 request.user.has_perm(perm))
