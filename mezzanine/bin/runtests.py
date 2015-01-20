23b82650317d320d0f5e5985875184b7927e76bf
Alex Hill <alex@hill.net.au>
2015-01-21 00:40:22 +0800

    Refactor runtests.py to work with DiscoverRunner

--- mezzanine/bin/runtests.py
+++ mezzanine/bin/runtests.py
@@ -18,32 +18,37 @@ def main(package="mezzanine"):
     package_path = path_for_import(package)
     project_path = os.path.join(package_path, "project_template")
 
-    local_settings_path = os.path.join(project_path,
-                                       "local_settings.py.template")
     test_settings_path = os.path.join(project_path, "test_settings.py")
 
     sys.path.insert(0, package_path)
     if not os.path.exists(test_settings_path):
-        test_reqs_str = """
+        # Import all our normal settings, and make a few test-specific tweaks.
+        # require Mezzanine's accounts app, use the md5 password hasher to
+        # speed up tests, use in-memory databases, and define an "other" db.
+        test_settings = """
 from project_template import settings
-globals().update(settings.__dict__)
+
+globals().update(i for i in settings.__dict__.items() if i[0].isupper())
+
+# Require the mezzanine.accounts app. We use settings.INSTALLED_APPS here so
+# the syntax test doesn't complain about an undefined name.
 if "mezzanine.accounts" not in settings.INSTALLED_APPS:
     INSTALLED_APPS = list(settings.INSTALLED_APPS) + ["mezzanine.accounts"]
-"""
-        # MD5 password hasher speeds up tests, and Django requires an "other"
-        # database for its tests. Also, these databases are in-memory.
-        django_default_settings = """
+
+# Use in-memory databases, and define an "other" db to make Django happy.
 DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'},
              'other': {'ENGINE': 'django.db.backends.sqlite3'}}
+
+# Use the md5 password hasher for quicker test runs.
 PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
+
+# These just need to be defined as something.
+SECRET_KEY = "django_tests_secret_key"
+NEVERCACHE_KEY = "django_tests_nevercache_key"
 """
-        with open(local_settings_path, "r") as f:
-            local_settings = f.read()
 
         with open(test_settings_path, "w") as f:
-            f.write("\n".join(
-                (test_reqs_str, local_settings, django_default_settings)
-            ))
+            f.write(test_settings)
 
         def cleanup_test_settings():
             os.remove(test_settings_path)
