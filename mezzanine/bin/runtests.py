b66835fb315f1d7f063a0c533fe0b643d3e24bda
Alex Hill <alex@hill.net.au>
2015-01-20 16:31:02 +0800

    Include Django's default settings when running tests

--- mezzanine/bin/runtests.py
+++ mezzanine/bin/runtests.py
@@ -19,6 +19,10 @@ def main(package="mezzanine"):
     package_path = path_for_import(package)
     project_path = os.path.join(package_path, "project_template")
 
+    django_path = os.path.dirname(os.path.abspath(django.__file__))
+    django_test_settings_path = os.path.join(django_path,
+                                             '../tests/test_sqlite.py')
+
     local_settings_path = os.path.join(project_path, "local_settings.py")
     test_settings_path = os.path.join(project_path, "test_settings.py")
 
@@ -28,6 +32,9 @@ def main(package="mezzanine"):
         shutil.copy(local_settings_path + ".template", test_settings_path)
         with open(test_settings_path, "r") as f:
             local_settings = f.read()
+        with open(django_test_settings_path, "r") as f:
+            django_test_settings = '\n'.join(line for line in f.readlines()
+                                             if not line.startswith('#'))
         with open(test_settings_path, "w") as f:
             test_reqs_str = """
 from project_template import settings
@@ -35,8 +42,12 @@ globals().update(settings.__dict__)
 if "mezzanine.accounts" not in settings.INSTALLED_APPS:
     INSTALLED_APPS = list(settings.INSTALLED_APPS) + ["mezzanine.accounts"]
 """
-            f.write(test_reqs_str + local_settings)
-        atexit.register(lambda: os.remove(test_settings_path))
+            f.write(test_reqs_str + local_settings + django_test_settings)
+
+        def cleanup_test_settings():
+            os.remove(test_settings_path)
+            os.remove(test_settings_path + 'c')
+        atexit.register(cleanup_test_settings)
 
     if django.VERSION >= (1, 7):
         django.setup()
