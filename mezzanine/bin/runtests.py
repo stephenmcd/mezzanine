ab5d715b0b8dafd9567b9208f1ece6c8eb966c9d
Alex Hill <alex@hill.net.au>
2015-01-20 16:48:59 +0800

    Hard-code Django's default test settings

--- mezzanine/bin/runtests.py
+++ mezzanine/bin/runtests.py
@@ -19,30 +19,33 @@ def main(package="mezzanine"):
     package_path = path_for_import(package)
     project_path = os.path.join(package_path, "project_template")
 
-    django_path = os.path.dirname(os.path.abspath(django.__file__))
-    django_test_settings_path = os.path.join(django_path,
-                                             '../tests/test_sqlite.py')
-
-    local_settings_path = os.path.join(project_path, "local_settings.py")
+    local_settings_path = os.path.join(project_path,
+                                       "local_settings.py.template")
     test_settings_path = os.path.join(project_path, "test_settings.py")
 
     sys.path.insert(0, package_path)
     sys.path.insert(0, project_path)
     if not os.path.exists(test_settings_path):
-        shutil.copy(local_settings_path + ".template", test_settings_path)
-        with open(test_settings_path, "r") as f:
-            local_settings = f.read()
-        with open(django_test_settings_path, "r") as f:
-            django_test_settings = '\n'.join(line for line in f.readlines()
-                                             if not line.startswith('#'))
-        with open(test_settings_path, "w") as f:
-            test_reqs_str = """
+        test_reqs_str = """
 from project_template import settings
 globals().update(settings.__dict__)
 if "mezzanine.accounts" not in settings.INSTALLED_APPS:
     INSTALLED_APPS = list(settings.INSTALLED_APPS) + ["mezzanine.accounts"]
 """
-            f.write(test_reqs_str + local_settings + django_test_settings)
+        # MD5 password hasher speeds up tests, and Django requires an "other"
+        # database for its tests. Also, these databases are in-memory.
+        django_default_settings = """
+DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'},
+             'other': {'ENGINE': 'django.db.backends.sqlite3',}}
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
+"""
+        with open(local_settings_path, "r") as f:
+            local_settings = f.read()
+
+        with open(test_settings_path, "w") as f:
+            f.write("\n".join(
+                (test_reqs_str, local_settings, django_default_settings)
+            ))
 
         def cleanup_test_settings():
             os.remove(test_settings_path)
