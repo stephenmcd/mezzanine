4d9db202d9bd141bf52a70c5e66b101840be1fee
Stephen McDonald <steve@jupo.org>
2015-01-25 13:39:18 +1100

    Restore previous behaviour in runtests script - use the local_settings template to create test_settings, not an actual local_settings module, which may already exist during development.

--- mezzanine/bin/runtests.py
+++ mezzanine/bin/runtests.py
@@ -21,18 +21,18 @@ def main(package="mezzanine"):
     package_path = path_for_import(package)
     project_path = os.path.join(package_path, "project_template")
 
-    # Create local_settings.py so it is imported by settings.py
     local_settings_path = os.path.join(project_path, "local_settings.py")
-    shutil.copy(local_settings_path + ".template", local_settings_path)
-
     test_settings_path = os.path.join(project_path, "test_settings.py")
 
     sys.path.insert(0, package_path)
+    sys.path.insert(0, project_path)
     if not os.path.exists(test_settings_path):
-        # Import all our normal settings, and make a few test-specific tweaks.
-        # require Mezzanine's accounts app, use the md5 password hasher to
-        # speed up tests, use in-memory databases, and define an "other" db.
-        test_settings = """
+        shutil.copy(local_settings_path + ".template", test_settings_path)
+        with open(test_settings_path, "r") as f:
+            local_settings = f.read()
+        with open(test_settings_path, "w") as f:
+            test_settings = """
+
 from project_template import settings
 
 globals().update(i for i in settings.__dict__.items() if i[0].isupper())
@@ -42,22 +42,15 @@ globals().update(i for i in settings.__dict__.items() if i[0].isupper())
 if "mezzanine.accounts" not in settings.INSTALLED_APPS:
     INSTALLED_APPS = list(settings.INSTALLED_APPS) + ["mezzanine.accounts"]
 
-# Use an in-memory database for tests.
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
+# Use the MD5 password hasher by default for quicker test runs.
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
 
-# Use the MD5 password hasher by default for quicker test runs. SHA1 still
-# needs to be turned on for the contrib.auth tests to pass in Django 1.4.
-PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',
-                    'django.contrib.auth.hashers.SHA1PasswordHasher')
 """
-
-        with open(test_settings_path, "w") as f:
-            f.write(test_settings)
+            f.write(test_settings + local_settings)
 
         def cleanup_test_settings():
-            for fn in [test_settings_path, local_settings_path]:
+            for fn in [test_settings_path, test_settings_path + 'c']:
                 os.remove(fn)
-                os.remove(fn + 'c')
         atexit.register(cleanup_test_settings)
 
     if django.VERSION >= (1, 7):
