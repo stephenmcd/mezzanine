92fcb384cfd3e2ba6f66842994a30a2ccff1b385
Stephen McDonald <steve@jupo.org>
2015-05-13 08:09:44 +1000

    Fix missing bracket

--- mezzanine/accounts/forms.py
+++ mezzanine/accounts/forms.py
@@ -18,7 +18,7 @@ from mezzanine.utils.urls import slugify, unique_slug
 User = get_user_model()
 
 _exclude_fields = tuple(getattr(settings,
-                                "ACCOUNTS_PROFILE_FORM_EXCLUDE_FIELDS", ())
+                                "ACCOUNTS_PROFILE_FORM_EXCLUDE_FIELDS", ()))
 
 # If a profile model has been configured with the ``AUTH_PROFILE_MODULE``
 # setting, create a model form for it that will have its fields added to
