eafbf4b385adfd5a183196a357e94391b6c33854
Alex Hill <alex@hill.net.au>
2015-06-10 14:35:21 +0800

    Use real_project_name in wsgi.py

--- mezzanine/project_template/project_name/wsgi.py
+++ mezzanine/project_template/project_name/wsgi.py
@@ -10,8 +10,9 @@ https://docs.djangoproject.com/en/{{ docs_version }}/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from mezzanine.utils.conf import real_project_name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "{{ project_name }}.settings")
+                      "%s.settings" % real_project_name("{{ project_name }}")
 
 application = get_wsgi_application()
