e7929edaf00f009f8f64dac542d869fc55a1df6e
Alex Hill <alex@hill.net.au>
2015-06-10 14:42:53 +0800

    Add missing trailing bracket

--- mezzanine/project_template/project_name/wsgi.py
+++ mezzanine/project_template/project_name/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("{{ project_name }}")
+                      "%s.settings" % real_project_name("{{ project_name }}"))
 
 application = get_wsgi_application()
