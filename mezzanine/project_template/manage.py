8ae404d4f1db3cfbfc79c54c06cbacfb723ac245
Alex Hill <alex@hill.net.au>
2015-06-10 14:30:27 +0800

    Use real_project_name in manage.py

--- mezzanine/project_template/manage.py
+++ mezzanine/project_template/manage.py
@@ -3,8 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                          "{{ project_name }}.settings")
+    
+    from mezzanine.utils.conf import real_project_name
+
+    settings_module = "%s.settings" % real_project_name("{{ project_name }}")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
     from django.core.management import execute_from_command_line
 
