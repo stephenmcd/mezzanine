c9c8580eb6b73de035ff7cbe2e9142b88184cc01
Andrzej Grymkowski <grymek4@gmail.com>
2015-07-06 20:34:35 +0200

    added tests

--- mezzanine/api/tests.py
+++ mezzanine/api/tests.py
@@ -1,8 +1,11 @@
 from __future__ import unicode_literals
 
+import json
 import re
+
 from unittest import skipUnless
 from datetime import datetime
+
 from django.core.urlresolvers import reverse
 
 from mezzanine.blog.models import BlogPost, BlogCategory
@@ -17,10 +20,35 @@ from mezzanine.utils.tests import TestCase
             "mezzanine.blog" in settings.INSTALLED_APPS,
             "api and blog app required")
 class ApiBlogTest(TestCase):
+    """
 
+    .. note::
+        In order to prevent `InSecurePlatformWarning` is recommended
+        to use `requests==2.5.3`.
+        For more information about it:
+        https://urllib3.readthedocs.org/en/latest/security.html#insecureplatformwarning
+
+    """
     def parse_iso_datetime(self, iso_datetime):
+        """
+        Converts iso format datetime to datetime object.
+
+        :param iso_datetime: iso datetime like "2008-09-03T20:56:35.450686Z"
+        :rtype iso_datetime: str
+        :return: datetime object.
+        """
         r = re.split('[^\d]', iso_datetime)
         return datetime(*map(int, r[:-1]))
+
+    def get_json_response(self, url):
+        """
+        Do request from url and returns response in json.
+
+        :param url: url to do request
+        :return: response in json.
+        """
+        data = self.client.get(url).content
+        return json.loads(data)
 
     def setUp(self):
         super(ApiBlogTest, self).setUp()
@@ -45,36 +73,72 @@ class ApiBlogTest(TestCase):
 
         blog_post.save()
 
-    def test_blogpost_list(self):
-        response = self.client.get(reverse('blogpost-list'))
-        data = response.content
-        print data
-        print data[0]
+        blog_post_data = dict(
+            title="Post 2",
+            description="Post for test tags, category, author and month",
+            user=self._user,
+            status=CONTENT_STATUS_PUBLISHED,
+            keywords_string=','.join(["brown", "cow"])
+        )
+        blog_post = BlogPost.objects.create(**blog_post_data)
+        blog_post.categories.add(category)
+
+        keywords = ["brown", "cow"]
+        for keyword in keywords:
+            keyword_id = Keyword.objects.get_or_create(title=keyword)[0].id
+            blog_post.keywords.add(AssignedKeyword(keyword_id=keyword_id))
+
+        blog_post.save()
+
+    def test_blog_list(self):
+        data = self.get_json_response(reverse('blogpost-list'))
         post = data['results'][0]
-        self.assertEqual(post['title'], 'Post')
-        self.assertContains(post['keywords_string'], "brown")
+        self.assertEqual(post['title'], 'Post 2')
+        self.assertIn("brown", post['keywords_string'])
         self.assertEqual(len(post['categories']), 1)
 
     def test_blog_tags(self):
-        data = self.client.get(reverse('blogpost-posts-by-tags')).content
+        data = self.get_json_response(reverse('blogpost-posts-by-tags'))
+        # data:
+        # [{u'count': 1, u'slug': u'how', u'title': u'how'},
+        #  {u'count': 1, u'slug': u'now', u'title': u'now'},
+        #  {u'count': 2, u'slug': u'brown', u'title': u'brown'},
+        #  {u'count': 2, u'slug': u'cow', u'title': u'cow'}]
+
+        keywords = {}
+        for d in data:
+            keywords[d['slug']] = d['count']
+
+        self.assertEqual(keywords['how'], 1)
+        self.assertEqual(keywords['brown'], 2)
 
     def test_blog_months(self):
-        data = self.client.get(reverse('blogpost-posts-by-months')).content
+        data = self.get_json_response(reverse('blogpost-posts-by-months'))
+        # data: [[u'2015-07-01T00:00:00', 2]]
+
         self.assertGreater(len(data), 0,
                            'Should be at least one date from post')
 
         now = datetime.now()
-        print 'test_blog_months', data, data[0][0]
-        iso_datetime, count = tuple(data[0])
+        iso_datetime, count = data[0]
         iso_datetime = self.parse_iso_datetime(iso_datetime)
 
         self.assertEqual(iso_datetime.month, now.month)
-        self.assertEqual(count, 1)
+        self.assertEqual(count, 2)
 
     def test_blog_authors(self):
-        data = self.client.get(reverse('blogpost-posts-by-authors')).content
-        print 'test_blog_authors', data
+        data = self.get_json_response(reverse('blogpost-posts-by-authors'))
+        # data: [{u'count': 2, u'author': u'test'}]
+
+        self.assertEqual(data[0]['author'], self._user.username)
+        self.assertEqual(data[0]['count'], 2)
 
     def test_blog_categories(self):
-        data = self.client.get(reverse('blogpost-posts-by-categories')).content
-        print 'test_blog_categories', data
+        data = self.get_json_response(reverse('blogpost-posts-by-categories'))
+        # data:
+        # [{'count': 2, 'slug': 'blog-category', 'title': u'Blog Category'}]
+
+        data = data[0]
+        category = BlogCategory.objects.get(title='Blog Category')
+        self.assertEqual(data['count'], 2)
+        self.assertEqual(data['title'], category.title)
