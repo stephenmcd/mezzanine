7c4b82d914c51076971607316b72e367d762f883
Stephen McDonald <steve@jupo.org>
2015-07-28 00:07:56 +1000

    Force local_settings.py into sys.modules so it's visible to Django's autoreloader.

--- mezzanine/project_template/project_name/settings.py
+++ mezzanine/project_template/project_name/settings.py
@@ -297,9 +297,15 @@ OPTIONAL_APPS = (
 
 # Instead of doing "from .local_settings import *", we use exec so that
 # local_settings has full access to everything defined in this module.
+# Also force into sys.modules so it's visible to Django's autoreload.
 
 f = os.path.join(PROJECT_APP_PATH, "local_settings.py")
 if os.path.exists(f):
+    import sys, imp
+    module_name = "%s.local_settings" % PROJECT_APP
+    module = imp.new_module(module_name)
+    module.__file__ = f
+    sys.modules[module_name] = module
     exec(open(f, "rb").read())
 
 
