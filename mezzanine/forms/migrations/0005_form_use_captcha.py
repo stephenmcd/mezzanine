555508d843e13fb73aa1fed03433f201c7284b58
eino <eino.makitalo@netitbe.fi>
2015-09-29 11:18:34 +0300

    default False -> not using Google reCaptcha

--- mezzanine/forms/migrations/0005_form_use_captcha.py
+++ mezzanine/forms/migrations/0005_form_use_captcha.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='form',
             name='use_captcha',
-            field=models.BooleanField(default=True, help_text='Prevent form submit (and email processing) if captcha not OK', verbose_name='Require captcha'),
+            field=models.BooleanField(default=False, help_text='Prevent form submit (and email processing) if captcha not OK', verbose_name='Require captcha'),            
         ),
     ]
