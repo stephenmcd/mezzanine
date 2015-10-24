a9a651762e289c17e7052a623689dd4457aee4b3
rahul754 <rahul.shukla754@gmail.com>
2015-10-24 21:53:11 +0530

    add templates

---
+++ mozz/manage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+
+    from mezzanine.utils.conf import real_project_name
+
+    settings_module = "%s.settings" % real_project_name("mozz")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
