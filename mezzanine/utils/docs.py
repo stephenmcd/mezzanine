e83c1de287883a1d66b9c772d1f2f65338e05cf1
T.Chmelevskij <T.Chmelevskij@gmail.com>
2015-12-11 11:19:43 +0000

    Import ascii letters as letters
    When importing ascii letters use them as letters, also remove trailing
    whitespaces.

--- mezzanine/utils/docs.py
+++ mezzanine/utils/docs.py
@@ -8,7 +8,7 @@ from future.builtins import map, open, str
 from datetime import datetime
 import os.path
 from shutil import copyfile, move
-from string import ascii_letters
+from string import ascii_letters as letters
 from socket import gethostname
 from warnings import warn
 
@@ -58,7 +58,7 @@ def build_settings_docs(docs_path, prefix=None):
                 setting_default = dynamic
         if setting_default != dynamic:
             setting_default = repr(deep_force_unicode(setting_default))
-        lines.extend(["", settings_label]) 
+        lines.extend(["", settings_label])
         lines.extend(["", settings_name, "-" * len(settings_name)])
         lines.extend(["",
             urlize(setting["description"] or "").replace(
