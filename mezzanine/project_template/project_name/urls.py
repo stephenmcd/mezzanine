99e23d3314d161aa1fcb51a89a709cc5fb2e1134
Stephen McDonald <steve@jupo.org>
2016-01-04 15:20:35 +1100

    Remove deprecated use of i18n_patterns()

--- mezzanine/project_template/project_name/urls.py
+++ mezzanine/project_template/project_name/urls.py
@@ -15,7 +15,7 @@ admin.autodiscover()
 # You can also change the ``home`` view to add your own functionality
 # to the project's homepage.
 
-urlpatterns = i18n_patterns("",
+urlpatterns = i18n_patterns(
     # Change the admin prefix here to use an alternate URL for the
     # admin interface, which would be marginally more secure.
     url("^admin/", include(admin.site.urls)),
