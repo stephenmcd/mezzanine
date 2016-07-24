9162ce9d48b925f83b157e8786093cb24a9b39e4
Alex Hill <alex@hill.net.au>
2016-07-25 01:34:32 +0800

    Allow Django 1.10 in setup.py

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ try:
         packages=find_packages(),
         install_requires=[
             "django-contrib-comments",
-            "django >= 1.8, < 1.10",
+            "django >= 1.8, < 1.11",
             "filebrowser_safe >= 0.4.1",
             "grappelli_safe >= 0.4.2",
             "tzlocal >= 1.0",
