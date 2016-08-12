be0abd9df3b2c9b22b776091f14c16c33f427fe2
Stephen McDonald <steve@jupo.org>
2016-08-12 11:21:49 +1000

    Restore access to parent template context in comments template tag. Closes #1654.

--- mezzanine/generic/templatetags/comment_tags.py
+++ mezzanine/generic/templatetags/comment_tags.py
@@ -24,13 +24,14 @@ def comments_for(context, obj):
     form_class = import_dotted_path(settings.COMMENT_FORM_CLASS)
     form = form_class(context["request"], obj)
     context_form = context.get("posted_comment_form", form)
-    return {
+    context.update({
         'posted_comment_form':
             context_form if context_form.target_object == obj else form,
         'unposted_comment_form': form,
         'comment_url': reverse("comment"),
         'object_for_comments': obj,
-    }
+    })
+    return context
 
 
 @register.inclusion_tag("generic/includes/comment.html", takes_context=True)
