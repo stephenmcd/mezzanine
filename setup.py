babbde76e94eea6466b5e990d26ab8c4fa5edbb2
Stephen McDonald <steve@jupo.org>
2016-08-16 09:42:32 +1000

    Bump dependencies.

--- setup.py
+++ setup.py
@@ -52,8 +52,8 @@ try:
         install_requires=[
             "django-contrib-comments",
             "django >= 1.8, < 1.11",
-            "filebrowser_safe >= 0.4.1",
-            "grappelli_safe >= 0.4.2",
+            "filebrowser_safe >= 0.4.4",
+            "grappelli_safe >= 0.4.3",
             "tzlocal >= 1.0",
             "bleach >= 1.4",
             "beautifulsoup4 >= 4.1.3",
