6a6cfa2e75d88f05f52964c704afc65af71670c1
ryneeverett <ryneeverett@gmail.com>
2016-09-15 21:08:51 -0400

    Deprecate SSLRedirectMiddleware. Resolve #1600.
    Also, set default SSL settings to `editable=False` so they do not
    display in the settings by default.

--- mezzanine/core/middleware.py
+++ mezzanine/core/middleware.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import warnings
+
 from django.contrib import admin
 from django.contrib.auth import logout
 from django.contrib.messages import error
@@ -221,6 +223,13 @@ class SSLRedirectMiddleware(MiddlewareMixin):
     Also ensure URLs defined by ``SSL_FORCE_URL_PREFIXES`` are redirect
     to HTTPS, and redirect all other URLs to HTTP if on HTTPS.
     """
+    def __init__(self, get_response):
+        warnings.warn(
+            "SSLRedirectMiddleware is deprecated. See "
+            "https://docs.djangoproject.com/en/stable/ref/middleware/"
+            "#module-django.middleware.security for alternative solutions.",
+            DeprecationWarning)
+        super(SSLRedirectMiddleware, self).__init__(get_response)
 
     def languages(self):
         if not hasattr(self, "_languages"):
--- mezzanine/project_template/project_name/settings.py
+++ mezzanine/project_template/project_name/settings.py
@@ -273,8 +273,6 @@ MIDDLEWARE_CLASSES = (
     "mezzanine.core.middleware.TemplateForHostMiddleware",
     "mezzanine.core.middleware.AdminLoginInterfaceSelectorMiddleware",
     "mezzanine.core.middleware.SitePermissionMiddleware",
-    # Uncomment the following if using any of the SSL settings:
-    # "mezzanine.core.middleware.SSLRedirectMiddleware",
     "mezzanine.pages.middleware.PageMiddleware",
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
 )
