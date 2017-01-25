6163192f46f6a7ec7262a8e17ecf945473080519
Ernst Sjöstrand <ernstp@gmail.com>
2017-01-25 04:57:38 +0100

    Fix issue with PyPy2 (#1725)
    Under PyPy2 you can't do u"foo" == lazy(static, str)("bar") because the
    code assumes dir(str) is a strict subset of dir(unicode), which isn't
    true on PyPy2. The other way around is no problem however, and the
    other strings in the static assets lists are unicode anyway.

--- mezzanine/utils/static.py
+++ mezzanine/utils/static.py
@@ -5,7 +5,8 @@ from __future__ import unicode_literals
 
 from django.contrib.admin.templatetags.admin_static import static
 from django.utils.functional import lazy
+from django.utils import six
 
 # The 'static' template tag returns cache-busting file names, which prevents
 # CDN's or browsers from serving old assets.
-static_lazy = lazy(static, str)
+static_lazy = lazy(static, six.text_type)
