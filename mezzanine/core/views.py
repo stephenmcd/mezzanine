35bba4449b712ac39dc204b571a5c81d2494a892
Peter Dermott <p.dermott@fermorltd.co.uk>
2017-04-07 11:04:19 +0100

    Try and fix admin link error mk2

--- mezzanine/core/views.py
+++ mezzanine/core/views.py
@@ -202,7 +202,7 @@ def displayable_links_js(request):
         if real:
             verbose_name = _("Page") if page else obj._meta.verbose_name
             title = "%s: %s" % (verbose_name, title)
-        if url is not None:
+        if url is not None and str(title) is not None:
             links.append((not page and real, {"title": str(title), "value": url}))
     sorted_links = sorted(links, key=lambda link: (link[0], link[1]['value']))
     return HttpResponse(dumps([link[1] for link in sorted_links]))
