b82f77d878388c8bb62c2fabca319c659259ab41
Samir Shah <samir@regulusweb.com>
2017-04-21 06:24:12 +0300

    Add explicit on_delete arguments to foreign keys in migrations.

--- mezzanine/pages/migrations/0001_initial.py
+++ mezzanine/pages/migrations/0001_initial.py
@@ -46,7 +46,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Link',
             fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page', on_delete=models.CASCADE)),
             ],
             options={
                 'ordering': ('_order',),
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='RichTextPage',
             fields=[
-                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page')),
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='pages.Page', on_delete=models.CASCADE)),
                 ('content', mezzanine.core.fields.RichTextField(verbose_name='Content')),
             ],
             options={
@@ -71,13 +71,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='page',
             name='parent',
-            field=models.ForeignKey(related_name='children', blank=True, to='pages.Page', null=True),
+            field=models.ForeignKey(related_name='children', blank=True, to='pages.Page', null=True, on_delete=models.CASCADE),
             preserve_default=True,
         ),
         migrations.AddField(
             model_name='page',
             name='site',
-            field=models.ForeignKey(editable=False, to='sites.Site'),
+            field=models.ForeignKey(editable=False, to='sites.Site', on_delete=models.CASCADE),
             preserve_default=True,
         ),
     ]
