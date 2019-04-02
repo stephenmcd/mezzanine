b06637fdb3b40c5d85898cce53b4b869ab7aeacd
Peter Dermott <p.dermott@fermorltd.co.uk>
2019-04-02 13:05:55 +0100

    Add messages to installed apps

--- mezzanine/project_template/project_name/settings.py
+++ mezzanine/project_template/project_name/settings.py
@@ -237,6 +237,7 @@ INSTALLED_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.messages",
     "django.contrib.redirects",
     "django.contrib.sessions",
     "django.contrib.sites",
