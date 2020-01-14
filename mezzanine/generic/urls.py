56061eb35d914fba315efcfb0a8459bbc8a9bbc0
Peter Dermott <p.dermott@fermorltd.co.uk>
2020-01-14 11:45:29 +0000

    2.0 compatible urls for generic

--- mezzanine/generic/urls.py
+++ mezzanine/generic/urls.py
@@ -1,11 +1,9 @@
-from __future__ import unicode_literals
-
-from django.conf.urls import url
+from django.urls import path
 
 from mezzanine.generic import views
 
 
 urlpatterns = [
-    url(r"^rating/$", views.rating, name="rating"),
-    url(r"^comment/$", views.comment, name="comment"),
+    path("rating/", views.rating, name="rating"),
+    path("comment/", views.comment, name="comment"),
 ]
