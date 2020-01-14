2d0bf1df3d17e58b1bd27e67c549a44bcf6d2099
Peter Dermott <p.dermott@fermorltd.co.uk>
2020-01-14 11:33:06 +0000

    Remove future imports for conf

--- mezzanine/conf/models.py
+++ mezzanine/conf/models.py
@@ -1,13 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
 
 from mezzanine.core.models import SiteRelated
 
 
-@python_2_unicode_compatible
 class Setting(SiteRelated):
     """
     Stores values for ``mezzanine.conf`` that can be edited via the admin.
