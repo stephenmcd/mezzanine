8e6dc3e4db7ad55a64904a612fd46b47fe057863
Peter Dermott <p.dermott@fermorltd.co.uk>
2020-01-14 13:19:08 +0000

    Revert "Move jQuery setup for ajax/csrf outside of document.ready handler."
    This reverts commit 543c4374

--- mezzanine/core/static/mezzanine/js/admin/ajax_csrf.js
+++ mezzanine/core/static/mezzanine/js/admin/ajax_csrf.js
@@ -1,11 +1,8 @@
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-jQuery.ajaxSetup({
-    beforeSend: function(xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", window.__csrf_token);
+
+jQuery(function($) {
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            xhr.setRequestHeader('X-CSRFToken', window.__csrf_token);
         }
-    }
+    });
 });
