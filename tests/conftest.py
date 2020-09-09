1a818fa511bdc07eb438b842e2ad2ab4fd38b840
Ed Rivas <ed@unplug.studio>
2020-09-09 13:28:44 -0600

    tests: use temp dirs to support parallel testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
 import os
 import shutil
 import sys
+import tempfile
 
 import django
 
 from pathlib import Path
 
-# Path to the temp mezzanine project folder (will be a sibling of this file)
-PROJECT_PATH = Path(__file__).parent / "project_name"
+# Path to the temp mezzanine project folder
+TMP_PATH = Path(tempfile.mkdtemp()) / "project_template"
 
 TEST_SETTINGS = """
 from . import settings
@@ -28,9 +29,9 @@ PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
 
 def pytest_report_header(config):
     """
-    Have pytest report the path to the project folder
+    Have pytest report the path of the project folder
     """
-    return f"mezzanine proj (tmp): {PROJECT_PATH}"
+    return f"mezzanine proj (tmp): {TMP_PATH}"
 
 
 def pytest_configure():
@@ -39,24 +40,23 @@ def pytest_configure():
     """
     from mezzanine.utils.importing import path_for_import
 
-    template_path = (
-        Path(path_for_import("mezzanine")) / "project_template" / "project_name"
-    )
-    shutil.copytree(str(template_path), str(PROJECT_PATH))
-    local_settings = (PROJECT_PATH / "local_settings.py.template").read_text()
-    (PROJECT_PATH / "test_settings.py").write_text(TEST_SETTINGS + local_settings)
+    template_path = Path(path_for_import("mezzanine")) / "project_template"
+    shutil.copytree(str(template_path), str(TMP_PATH))
+    proj_path = TMP_PATH / "project_name"
+    local_settings = (proj_path / "local_settings.py.template").read_text()
+    (proj_path / "test_settings.py").write_text(TEST_SETTINGS + local_settings)
 
     # Setup the environment for Django
-    sys.path.insert(0, str(PROJECT_PATH.parent))
+    sys.path.insert(0, str(TMP_PATH))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.test_settings")
     django.setup()
 
 
 def pytest_unconfigure():
     """
-    Remove the folder folder we copied in `pytest_configure`.
+    Remove the temporary folder
     """
     try:
-        shutil.rmtree(str(PROJECT_PATH))
+        shutil.rmtree(str(TMP_PATH))
     except OSError:
         pass
