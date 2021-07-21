95b138a961d5a102cfe79ed792cba1e9b93d134a
Ed Rivas <ed@jerivas.com>
2021-07-21 17:36:51 -0600

    fix: override Django's collapse.js
    Instead of competing with it. Should also fix #1987.

--- mezzanine/core/static/admin/js/collapse.js
+++ mezzanine/core/static/admin/js/collapse.js
@@ -2,15 +2,15 @@
 jQuery(function($) {
 
     /// FIELDSETS
-    $('fieldset[class*="collapse"]').each(function() {
+    $('fieldset.collapse').each(function() {
         $(this).addClass("collapse-open");
         $(this).removeClass("collapse");
     });
-    $('fieldset[class*="collapse-closed"]').each(function() {
+    $('fieldset.collapse-closed').each(function() {
         $(this).addClass("collapsed");
         $(this).find('h2:first').addClass("collapse-toggle");
     });
-    $('fieldset[class*="collapse-open"]').each(function() {
+    $('fieldset.collapse-open').each(function() {
         $(this).find('h2:first').addClass("collapse-toggle");
     });
     $('h2.collapse-toggle').bind("click", function(e){
@@ -20,7 +20,7 @@ jQuery(function($) {
     });
 
     /// OPEN FIELDSETS WITH ERRORS
-    $('fieldset[class*="collapse-closed"]').children('div[class*="errors"]').each(function(i) {
+    $('fieldset.collapse-closed').children('div[class*="errors"]').each(function(i) {
         $(this).parent().toggleClass("collapsed");
         $(this).parent().toggleClass('collapse-closed');
         $(this).parent().toggleClass('collapse-open');
