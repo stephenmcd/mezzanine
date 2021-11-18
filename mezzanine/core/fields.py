e33ff627674434c6b3caf825d742b8641f3fdee9
aparakian <alexisparakian@gmail.com>
2021-11-18 17:48:41 +0100

    fix: fix infinite loop in value_to_string method (#1999)
    Co-authored-by: AbdellaouiSofiane <62038668+AbdellaouiSofiane@users.noreply.github.com>

--- mezzanine/core/fields.py
+++ mezzanine/core/fields.py
@@ -94,7 +94,7 @@ class MultiChoiceField(models.CharField):
             raise ValidationError(error)
 
     def value_to_string(self, obj):
-        value = self.value_to_string(obj)
+        value = self.value_from_object(obj)
         return ",".join(value)
 
 
