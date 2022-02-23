11e843d8f6d978196aa3b5a0ff84b937ab1b55db
Ed Rivas <ed@jerivas.com>
2022-02-23 14:39:00 -0600

    fix: don't enforce uniqueness of empty emails. Fixes #1984

--- mezzanine/core/admin.py
+++ mezzanine/core/admin.py
@@ -408,7 +408,8 @@ class SitePermissionInline(admin.TabularInline):
 class SitePermissionUserAdminForm(UserAdmin.form):
     def clean_email(form):
         email = form.cleaned_data.get("email")
-        if User.objects.exclude(id=form.instance.id).filter(email=email).exists():
+        same_email = User.objects.exclude(id=form.instance.id).filter(email=email)
+        if email and same_email.exists():
             raise ValidationError(_("This email is already registered"))
         return email
 
